5d51d607974d12c22d040ce7f1bcbd645c6628c4
LostCore <lostcore@gmail.com>
2014-05-30 17:19:43 +0200

    Correzioni al Nav Walker (ora per i sottomenu stampa un <b class="arrow-right"> dopo il link, al posto di <b class="caret">

--- inc/WabootNavMenuWalker.php
+++ inc/WabootNavMenuWalker.php
@@ -4,6 +4,12 @@ require_once("vendor/BootstrapNavMenuWalker.php");
 
 class WabootNavMenuWalker extends BootstrapNavMenuWalker {
 
+    /**
+     * Starts a new <ul> element
+     * @param string $output
+     * @param int $depth
+     * @param array $args
+     */
     function start_lvl( &$output, $depth = 0, $args = array() ) {
 
         $padding = 10 * $depth;
@@ -12,7 +18,7 @@ class WabootNavMenuWalker extends BootstrapNavMenuWalker {
         $indent = str_repeat( "\t\t\t\t", $depth );
         $submenu = ($depth > 0) ? ' sub-menu' : '';
 
-        $output	   .= "\n$indent<ul class=\"dropdown-menu$submenu depth_$depth\">\n";
+        $output	   .= "\n".$indent."<ul class='dropdown-menu ".$submenu." depth_".$depth."'>\n";
 
         /*if($depth == 0)
             $output	   .= "\n$indent<ul class=\"dropdown-menu$submenu depth_$depth\">\n";
@@ -21,14 +27,22 @@ class WabootNavMenuWalker extends BootstrapNavMenuWalker {
 
     }
 
+    /**
+     * Starts a new <li><a> elements
+     * @param string $output Passed by reference. Used to append additional content.
+     * @param object $item   Menu item data object.
+     * @param int    $depth  Depth of menu item. Used for padding.
+     * @param array(in reality this is a stdClass) $args  An array of arguments. @see wp_nav_menu()
+     * @param int    $id     Current item ID.
+     */
     function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
 
         $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
 
-        $li_attributes = '';
-        $class_names = $value = '';
-
-        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+        /**
+         * <li> element
+         */
+        $classes = empty( $item->classes ) ? array() : (array) $item->classes; //$item->classes contains wordpress-given menu item classes
 
         // managing divider: add divider class to an element to get a divider before it.
         $divider_class_position = array_search('divider', $classes);
@@ -40,31 +54,41 @@ class WabootNavMenuWalker extends BootstrapNavMenuWalker {
         $classes[] = ($args->has_children) ? 'dropdown' : '';
         $classes[] = ($item->current || $item->current_item_ancestor) ? 'active' : '';
         $classes[] = 'menu-item-' . $item->ID;
-        if($depth && $args->has_children){
+        if($depth > 0 && $args->has_children){
             $classes[] = 'dropdown-submenu';
         }
 
 
-        $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+        $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) ); //array_filter( $classes ) will remove any empty or false element
         $class_names = ' class="' . esc_attr( $class_names ) . '"';
 
         $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
         $id = strlen( $id ) ? ' id="' . esc_attr( $id ) . '"' : '';
 
-        $output .= $indent . '<li' . $id . $value . $class_names . $li_attributes . '>';
+        $output .= $indent . '<li' . $id . $class_names . '>';
 
+        /**
+         * <a> element
+         */
         $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
         $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
         $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
         $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
-        $attributes .= ($args->has_children) 	    ? ' class="dropdown-toggle" data-toggle="dropdown"' : '';
+        if($args->has_children){
+            $attributes .= ' class="dropdown-toggle" data-toggle="dropdown"';
+        }
 
         $item_output = $args->before;
         $item_output .= '<a'. $attributes .'>';
         $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
-        $item_output .= $args->has_children ? ' <b class="caret"></b></a>' : '</a>';
+        if($args->has_children && $depth == 0){
+            $item_output .= '<b class="caret"></b></a>'; //first level <li><a> with submenus
+        }elseif($args->has_children && $depth > 0){
+            $item_output .= '<b class="arrow-right"></b></a>'; //subsequent levels of <li><a> with submenus
+        }else{
+            $item_output .= '</a>';
+        }
         $item_output .= $args->after;
-
 
         $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
     }
