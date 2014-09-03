c2cdd8f04d9b77d1fd78c46e9d31774b1c211934
LostCore <lostcore@gmail.com>
2014-09-03 16:10:04 +0200

    Cancellato tutto dal master!

--- core/inc/menus.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Register the navigation menus. This theme uses wp_nav_menu() in three locations.
- */
-register_nav_menus( array(
-	'top'           => __( 'Top Menu', 'alienship' ),
-	'main'          => __( 'Main Menu', 'alienship' ),
-	'bottom'        => __( 'Bottom Menu', 'alienship' )
-) );
