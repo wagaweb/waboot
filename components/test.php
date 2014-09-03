7bf137c4ea96832cb4754d3a394c9e7acbed6126
LostCore <lostcore@gmail.com>
2014-09-03 16:23:05 +0200

    Initial

---
+++ components/test.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+Plugin Name: Test Component
+Description: Its a sample component
+Version: 1.0
+Author: WAGA Team <dev@waga.it>
+Author URI: http://www.waga.it
+*/
--- functions.php
+++ functions.php
@@ -42,6 +42,9 @@ if ( ! function_exists( 'waboot_setup' ) ):
         // Load theme options framework
         locate_template( '/admin/options-panel.php', true );
 
+        // Load components framework
+        locate_template( '/admin/waboot-components-framework.php', true );
+
         // Customizer
         locate_template( '/inc/customizer.php', true );
 
@@ -85,6 +88,10 @@ if ( ! function_exists( 'waboot_setup' ) ):
         // Load Jetpack related support if needed.
         if ( class_exists( 'Jetpack' ) )
             locate_template( '/inc/jetpack.php', true );
+
+        //Loads components
+        Waboot_ComponentsManager::init();
+        Waboot_ComponentsManager::setupRegisteredComponents();
     }
 endif;
 add_action( 'after_setup_theme', 'waboot_setup' );
