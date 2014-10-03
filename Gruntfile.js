c8b65e6c166743ea2cc6f13cefa04f75a934a502
LostCore <lostcore@gmail.com>
2014-10-03 09:44:10 +0200

    - Aggiunto generatore POT

--- Gruntfile.js
+++ Gruntfile.js
@@ -155,6 +155,32 @@ module.exports = function(grunt) {
                 ]
             }
         },
+        pot:{
+            options:{
+                text_domain: 'waboot',
+                dest: 'languages/',
+                keywords: [
+                    '__:1',
+                    '_e:1',
+                    '_x:1,2c',
+                    'esc_html__:1',
+                    'esc_html_e:1',
+                    'esc_html_x:1,2c',
+                    'esc_attr__:1',
+                    'esc_attr_e:1',
+                    'esc_attr_x:1,2c',
+                    '_ex:1,2c',
+                    '_n:1,2',
+                    '_nx:1,2,4c',
+                    '_n_noop:1,2',
+                    '_nx_noop:1,2,3c'
+                ]
+            },
+            files:{
+                src: ['**/*.php'],
+                expand: true
+            }
+        },
         compress:{
             build:{
                 options:{
