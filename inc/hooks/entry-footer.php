0e4ac68dab534916b3a7d0ea4d9e1e2534e9b5b2
LostCore <lostcore@gmail.com>
2014-10-08 17:24:20 +0200

    Ripulito entry-footer.php

--- inc/hooks/entry-footer.php
+++ inc/hooks/entry-footer.php
@@ -5,19 +5,26 @@
  * @since 0.1.0
  *
  */
-function waboot_entry_footer_markup_open() {
-    echo '<footer class="entry-footer">';
-}
-add_action( 'waboot_entry_footer', 'waboot_entry_footer_markup_open', 5 );
 
-if ( ! function_exists( 'waboot_do_posted_on' ) ) :
+if(!function_exists( 'waboot_print_entry_footer')):
+    function waboot_print_entry_footer(){
+        echo '<footer class="entry-footer">';
+        waboot_do_posted_on();
+        waboot_do_post_author();
+        waboot_do_post_categories();
+        waboot_do_post_tags();
+        waboot_do_post_comments_link();
+        echo '</footer>';
+    }
+    add_action( 'waboot_entry_footer', 'waboot_print_entry_footer' );
+endif;
+
+if(!function_exists( 'waboot_do_posted_on')):
     /**
      * Prints HTML with date posted information for the current post.
-     *
      * @since 0.1.0
      */
     function waboot_do_posted_on() {
-
         // Return early if theme options are set to hide date
         if ( ! of_get_option( 'waboot_published_date', 1 ) )
             return;
@@ -29,17 +36,14 @@ if ( ! function_exists( 'waboot_do_posted_on' ) ) :
             esc_html( get_the_date() )
         );
     }
-    add_action( 'waboot_entry_footer', 'waboot_do_posted_on', 6 );
 endif;
 
-if ( ! function_exists( 'waboot_do_post_author' ) ) :
+if(!function_exists( 'waboot_do_post_author')):
     /**
      * Prints HTML with meta information for the current post's author.
-     *
      * @since 0.1.0
      */
     function waboot_do_post_author() {
-
         // Return early if theme options are set to hide author
         if ( ! of_get_option('waboot_post_author', 1 ) )
             return;
@@ -50,32 +54,29 @@ if ( ! function_exists( 'waboot_do_post_author' ) ) :
             esc_html( get_the_author() )
         );
     }
-    add_action( 'waboot_entry_footer', 'waboot_do_post_author', 7 );
 endif;
 
-if ( ! function_exists( 'waboot_do_post_categories' ) ):
+if(!function_exists( 'waboot_do_post_categories')):
     /**
      * Customize the list of categories displayed on index and on a post
      * @since 0.1.0
      */
     function waboot_do_post_categories() {
-
         // Return early if theme options are set to hide categories
         if ( ! of_get_option( 'waboot_post_categories', 1 ) )
             return;
 
         $post_categories = get_the_category();
-        if ( $post_categories ) {
-
+        if($post_categories){
             echo '<span class="cat-links"><i class="glyphicon glyphicon-folder-open" title="Categories"></i> ';
             $num_categories = count( $post_categories );
             $category_count = 1;
 
-            foreach ( $post_categories as $category ) {
+            foreach($post_categories as $category){
                 $html_before = '<a href="' . get_category_link( $category->term_id ) . '" class="cat-text">';
                 $html_after = '</a>';
 
-                if ( $category_count < $num_categories )
+                if ($category_count < $num_categories)
                     $sep = ', ';
                 elseif ( $category_count == $num_categories )
                     $sep = '';
@@ -85,27 +86,24 @@ if ( ! function_exists( 'waboot_do_post_categories' ) ):
             echo '</span>';
         }
     }
-    add_action( 'waboot_entry_footer', 'waboot_do_post_categories', 8 );
+    //add_action( 'waboot_entry_footer', 'waboot_do_post_categories', 8 );
 endif;
 
-if ( ! function_exists( 'waboot_do_post_tags' ) ):
+if(!function_exists( 'waboot_do_post_tags')):
     /**
      * Customize the list of tags displayed on index and on a post
      * @since 0.1.0
      */
     function waboot_do_post_tags() {
-
         // Return early if theme options are set to hide tags
         if ( ! of_get_option( 'waboot_post_tags', 1 ) )
             return;
 
         $post_tags = get_the_tags();
-        if ( $post_tags ) {
-
+        if($post_tags){
             echo '<span class="tags-links"><i class="glyphicon glyphicon-tags" title="Tags"></i> ';
             $num_tags = count( $post_tags );
             $tag_count = 1;
-
             foreach( $post_tags as $tag ) {
                 $html_before = '<a href="' . get_tag_link($tag->term_id) . '" rel="tag nofollow" class="tag-text">';
                 $html_after = '</a>';
@@ -121,35 +119,23 @@ if ( ! function_exists( 'waboot_do_post_tags' ) ):
             echo '</span>';
         }
     }
-    add_action( 'waboot_entry_footer', 'waboot_do_post_tags', 9 );
 endif;
 
-if ( ! function_exists( 'waboot_do_post_comments_link' ) ):
+if(!function_exists('waboot_do_post_comments_link')):
     /**
      * Display the "Leave a comment" message
      * @since 0.1.0
      */
     function waboot_do_post_comments_link() {
-
         // Return early if theme options are set to hide comment link
         if ( ! of_get_option( 'waboot_post_comments_link', 1 ) )
             return;
 
-        if ( comments_open() || ( '0' != get_comments_number() && ! comments_open() ) ) { ?>
-            <span class="comments-link">
+        if ( comments_open() || ( '0' != get_comments_number() && ! comments_open() ) ) : ?>
+        <span class="comments-link">
 			<i class="glyphicon glyphicon-comment"></i>
-                <?php comments_popup_link( __( ' Leave a comment', 'waboot' ), __( ' 1 Comment', 'waboot' ), __( ' % Comments', 'waboot' ) ); ?>
+            <?php comments_popup_link( __( ' Leave a comment', 'waboot' ), __( ' 1 Comment', 'waboot' ), __( ' % Comments', 'waboot' ) ); ?>
 		</span>
-        <?php }
+        <?php endif;
     }
-    add_action( 'waboot_entry_footer', 'waboot_do_post_comments_link', 15 );
 endif;
-
-/**
- * Print the closing markup for the entry header.
- * @since 0.1.0
- */
-function waboot_entry_footer_markup_close() {
-    echo '</footer>';
-}
-add_action( 'waboot_entry_footer', 'waboot_entry_footer_markup_close', 15 );
