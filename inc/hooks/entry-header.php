9b0e64a0a865728969f2075902422d0969965c5c
LostCore <lostcore@gmail.com>
2014-10-20 10:38:26 +0200

    - Pulito il codice in entry-header.php

--- inc/hooks/entry-header.php
+++ inc/hooks/entry-header.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * Set title to H1 if in single view, otherwise set it to H2
+ * @since 0.1.0
+ */
 function waboot_entry_title(){
 	$title = get_the_title();
 
@@ -14,20 +18,16 @@ function waboot_entry_title(){
 	return $str;
 }
 
-/**
- * Set title to H1 if in single view, otherwise set it to H2
- * @since 0.1.0
- */
 function waboot_print_entry_header() {
 	$str = '<header class="entry-header">';
 	$str .= waboot_entry_title();
 	$str .= '</header>';
-    echo apply_filters( 'waboot_entry_title_text', $str );
+    echo apply_filters( 'waboot_entry_header_text', $str );
 }
 add_action( 'waboot_entry_header', 'waboot_print_entry_header' );
 
 /**
- * Nasconde il titolo per post e pagine, utilizzando il modulo behavior
+ * Apply the "show-title" behavior to the entry header.
  * @param $title
  * @return string
  * @uses waboot_entry_title_text filter (inc/hooks.php::waboot_do_entry_title)
@@ -38,14 +38,12 @@ function waboot_title_toggler($title){
 
     $show_title = get_behavior("show-title","array");
 
-    if(!$show_title->is_enabled_for_current_node()){
-        return $title;
-    }else{
+    if($show_title->is_enabled_for_current_node()){
         if(is_singular() && ($show_title->value == "0" || !$show_title->value || $show_title->value == 0)){
             return "";
-        }else{
-            return $title;
         }
     }
+
+    return $title;
 }
-add_filter("waboot_entry_title_text","waboot_title_toggler");
+add_filter("waboot_entry_header_text","waboot_title_toggler");
