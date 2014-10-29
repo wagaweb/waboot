342ca65b47fc660bfb62cb8e6739ef776c4cfaa5
Fabio Procopio <fabio@waga.it>
2014-10-29 18:03:03 +0100

    Version 0.4.1 Fix Offcanvas Swipe

--- assets/js/waboot-mobile.js
+++ assets/js/waboot-mobile.js
@@ -12,20 +12,18 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
 // Inizialize fastclick to body
 jQuery(document).ready(function($) {
     FastClick.attach(document.body);
-    //if(isMobile()){
-    //Enable swiping...
     $("body").swipe({
-        //Generic swipe handler for all directions
         swipeRight: function(event, direction, distance, duration, fingerCount) {
-            $('button.navbar-toggle').trigger('click');
+            if ($(".navbar-mobile-collapse").css('right') == '0px') {
+                $('button.navbar-toggle').trigger('click');
+            }
         },
         swipeLeft: function(event, direction, distance, duration, fingerCount) {
-            $('button.navbar-toggle').trigger('click');
+            if ($(".navbar-mobile-collapse").css('right') == '0px') {
+                $('button.navbar-toggle').trigger('click');
+            }
         }
-        //Default is 75px, set to 0 for demo so any distance triggers swipe
-        // threshold:0
     });
     //Disable for Metaslider
     $(".metaslider").addClass("noSwipe");
-    //}
 });
