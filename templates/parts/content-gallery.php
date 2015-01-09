a6ca62e03cc11f2c015ae729eeb838b809779a25
Fabio Procopio <fabio@waga.it>
2015-01-09 18:53:28 +0100

    Fix Post Formats Part 2

--- templates/parts/content-gallery.php
+++ templates/parts/content-gallery.php
@@ -3,11 +3,11 @@
  * @package Waboot
  * @since Waboot 1.0
  */
-
 ?>
 <article role="article" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php do_action( 'waboot_entry_header' ); ?>
 	<div class="entry-content">
+		<?php waboot_gallery_format(); ?>
 		<?php the_content(); ?>
 		<?php wp_link_pages(); ?>
 	</div><!-- .entry-content -->
--- templates/parts/content-video.php
+++ templates/parts/content-video.php
@@ -7,7 +7,15 @@
 <article role="article" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <?php do_action( 'waboot_entry_header' ); ?>
 	<div class="entry-content">
-		<?php the_content(); ?>
+		<?php $first_video = et_get_first_video(); ?>
+		<?php if ( $first_video ) : ?>
+			<div class="wb-video-container">
+				<?php echo $first_video; ?>
+			</div>
+		<?php endif; ?>
+
+
+		<?php // the_content(); ?>
         <?php wp_link_pages(); ?>
 	</div><!-- .entry-content -->
 	<?php do_action( 'waboot_entry_footer' ); ?>
