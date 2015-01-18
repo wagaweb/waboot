257613992eb5aa4fa3796923a2aa0cbccd0018f9
LostCore <lostcore@gmail.com>
2015-01-18 12:33:51 +0100

    Aggiunto supporto alla traduzione del tema Aggiunto supporto html nativo di WP

--- functions.php
+++ functions.php
@@ -2,10 +2,12 @@
 
 locate_template('/wbf/wbf.php', true);
 locate_template('/inc/template-tags.php', true);
-init_style_compiler();
 
 if ( ! function_exists( 'waboot_setup' ) ):
     function waboot_setup() {
+        //Make theme available for translation.
+        load_theme_textdomain( 'waboot', get_template_directory() . '/languages' );
+
         // Custom hooks.
         locate_template( '/inc/hooks.php', true );
 
@@ -27,13 +29,14 @@ if ( ! function_exists( 'waboot_setup' ) ):
         // Load scripts
         locate_template( '/inc/scripts.php', true );
 
+        // Switch default core markup for search form, comment form, and comments to output valid HTML5.
+        add_theme_support( 'html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption') );
+
         // Add default posts and comments RSS feed links to head
         add_theme_support( 'automatic-feed-links' );
 
         // Add support for custom backgrounds
-        add_theme_support( 'custom-background', array(
-            'default-color' => 'ffffff',
-        ) );
+        add_theme_support( 'custom-background', array('default-color' => 'ffffff') );
 
         // Add support for post-thumbnails
         add_theme_support( 'post-thumbnails' );
@@ -51,6 +54,7 @@ add_action('after_setup_theme', 'waboot_setup', 11);
 /**
  * INIT STYLES COMPILER
  */
+if ( ! function_exists( 'init_style_compiler' ) ) :
 function init_style_compiler(){
 	$theme = waboot_get_compiled_stylesheet_name();
 	$GLOBALS['waboot_styles_compiler'] = new Waboot_Styles_Compiler(array(
@@ -64,3 +68,6 @@ function init_style_compiler(){
 		)
 	));
 }
+endif;
+
+init_style_compiler();
