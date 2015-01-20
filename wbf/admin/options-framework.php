2c56cae4ae0217f7a5bec7196911e984864141e2
LostCore <lostcore@gmail.com>
2015-01-20 13:00:14 +0100

    - Rivista la logica delle operazioni da effettuare al salvataggio delle theme options. Ora sono gestite da un'unica funzione. - Implementata la features che ricompila gli stili solo se sono state modificate opzioni che devono ricompilare gli stili.

--- wbf/admin/options-framework.php
+++ wbf/admin/options-framework.php
@@ -15,8 +15,7 @@ if ( STYLESHEETPATH == TEMPLATEPATH ) {
 }
 
 add_action( 'init', 'optionsframework_init', 20 );
-add_action( "updated_option", "of_style_options_save", 11, 3 );
-add_action( "updated_option", "of_compile_less_on_save", 9999, 3 );
+add_action( "updated_option", "of_options_save", 9999, 3 );
 
 function optionsframework_init() {
 	require WBF_DIRECTORY . '/vendor/options-framework/class-options-sanitization.php';
@@ -80,83 +79,96 @@ if ( ! function_exists( 'of_get_option' ) ) :
 	}
 endif;
 
-/**
- * Replace {of_get_option} tags in _theme-options-generated.less.cmp; It is called during "update_option" and only for of theme options.
- * @param string $option option name
- * @param $old_value old value of the option
- * @param $value new value of the option
- */
-function of_style_options_save($option, $old_value, $value){
+function of_options_save($option, $old_value, $value){
 	$config = get_option( 'optionsframework' );
 	if($option == $config['id']){
-		$tmpFile = new SplFileInfo(get_stylesheet_directory()."/sources/less/_theme-options-generated.less.cmp");
-		if(!$tmpFile->isFile() || !$tmpFile->isWritable()){
-			$tmpFile = new SplFileInfo(get_template_directory()."/sources/less/_theme-options-generated.less.cmp");
+		$must_recompile_flag = false;
+		$deps_to_achieve = array();
+		$all_options = Waboot_Options_Framework::_optionsframework_options();
+		$diff = array_diff_assoc($old_value,$value);
+
+		//Doing actions with modified options
+		foreach($all_options as $k => $opt_data){
+			if(isset($opt_data['id']) && array_key_exists($opt_data['id'],$diff)){ //True if the current option has been modified
+				if(isset($opt_data['recompile_styles']) && $opt_data['recompile_styles']){
+					$must_recompile_flag = true;
+				}
+				/*if(isset($opt_data['deps'])){
+					if(isset($opt_data['deps']['_global'])){
+						if($opt_data['deps']['_global']['components'])
+							$deps_to_achieve['components'][] = $opt_data['deps']['_global']['components'];
+					}
+				}*/
+			}
 		}
-		$parsedFile = new SplFileInfo(get_stylesheet_directory()."/sources/less/theme-options-generated.less");
-		if($tmpFile->isFile() && $tmpFile->isWritable()){
-			$genericOptionfindRegExp = "~//{of_get_option\('([a-zA-Z0-9\-_]+)'\)}~";
-			$fontOptionfindRegExp = "~//{of_get_font\('([a-zA-Z0-9\-_]+)'\)}~";
 
-			$tmpFileObj = $tmpFile->openFile("r");
-			$parsedFileObj = $parsedFile->openFile("w+");
-
-			while (!$tmpFileObj->eof()) {
-				$line = $tmpFileObj->fgets();
-				//Replace a generic of option
-				if(preg_match($genericOptionfindRegExp,$line,$matches)){
-					if(array_key_exists($matches[1],$value)){
-						if($value[$matches[1]] != ""){
-							$line = preg_replace($genericOptionfindRegExp,$value[$matches[1]],$line);
-						}else{
-							$line = "//{$matches[1]} is empty\n";
-						}
-					}else{
-						$line = "//{$matches[1]} not found\n";
-					}
-				}
-				//Replace a font option
-				if(preg_match($fontOptionfindRegExp,$line,$matches)){
-					$line = "//{$matches[1]} is empty\n";
-					if(array_key_exists($matches[1],$value)){
-						if($value[$matches[1]] != ""){
-							$attr = $value[$matches[1]];
-							$fontString = "font-family: '".$attr['family']."', ".$attr['category'].";";
-							/*if(preg_match("/([0-9]+)([a-z]+)/",$attr['style'],$style_matches)){
-								if($style_matches[1] == 'regular') $style_matches[1] = "normal";
-								$fontString .= "font-weight: ".$style_matches[1].";";
-								$fontString .= "font-style: ".$style_matches[2].";";
-							}else{
-								if($attr['style'] == 'regular') $attr['style'] = "normal";
-								$fontString .= "font-weight: ".$attr['style'].";";
-							}*/
-							$fontString .= "color: ".$attr['color'].";";
-							$line = $fontString;
-						}else{
-							$line = "//{$matches[1]} is empty\n";
-						}
-					}else{
-						$line = "//{$matches[1]} not found\n";
-					}
-				}
-				$parsedFileObj->fwrite($line);
+		if($must_recompile_flag){
+			_of_generate_less_file($value); //Create a _theme-options-generated.less file
+			//Then, compile less
+			if(isset($GLOBALS['waboot_styles_compiler'])){
+				global $waboot_styles_compiler;
+				$waboot_styles_compiler->compile();
 			}
 		}
 	}
 }
 
 /**
- * Compile LESS when theme options are saved. Called during "update_option"
- * @param $option
- * @param $old_value
- * @param $value
+ * Replace {of_get_option} and {of_get_font} tags in _theme-options-generated.less.cmp; It is called during "update_option" and only for of theme options.
+ * @param $value values of the options
  */
-function of_compile_less_on_save($option, $old_value, $value){
-	$config = get_option( 'optionsframework' );
-	if($option == $config['id']){
-		if(isset($GLOBALS['waboot_styles_compiler'])){
-			global $waboot_styles_compiler;
-			$waboot_styles_compiler->compile();
+function _of_generate_less_file($value){
+	$tmpFile = new SplFileInfo(get_stylesheet_directory()."/sources/less/_theme-options-generated.less.cmp");
+	if(!$tmpFile->isFile() || !$tmpFile->isWritable()){
+		$tmpFile = new SplFileInfo(get_template_directory()."/sources/less/_theme-options-generated.less.cmp");
+	}
+	$parsedFile = new SplFileInfo(get_stylesheet_directory()."/sources/less/theme-options-generated.less");
+	if($tmpFile->isFile() && $tmpFile->isWritable()) {
+		$genericOptionfindRegExp = "~//{of_get_option\('([a-zA-Z0-9\-_]+)'\)}~";
+		$fontOptionfindRegExp    = "~//{of_get_font\('([a-zA-Z0-9\-_]+)'\)}~";
+
+		$tmpFileObj    = $tmpFile->openFile( "r" );
+		$parsedFileObj = $parsedFile->openFile( "w+" );
+
+		while ( ! $tmpFileObj->eof() ) {
+			$line = $tmpFileObj->fgets();
+			//Replace a generic of option
+			if ( preg_match( $genericOptionfindRegExp, $line, $matches ) ) {
+				if ( array_key_exists( $matches[1], $value ) ) {
+					if ( $value[ $matches[1] ] != "" ) {
+						$line = preg_replace( $genericOptionfindRegExp, $value[ $matches[1] ], $line );
+					} else {
+						$line = "//{$matches[1]} is empty\n";
+					}
+				} else {
+					$line = "//{$matches[1]} not found\n";
+				}
+			}
+			//Replace a font option
+			if ( preg_match( $fontOptionfindRegExp, $line, $matches ) ) {
+				$line = "//{$matches[1]} is empty\n";
+				if ( array_key_exists( $matches[1], $value ) ) {
+					if ( $value[ $matches[1] ] != "" ) {
+						$attr       = $value[ $matches[1] ];
+						$fontString = "font-family: '" . $attr['family'] . "', " . $attr['category'] . ";";
+						/*if(preg_match("/([0-9]+)([a-z]+)/",$attr['style'],$style_matches)){
+							if($style_matches[1] == 'regular') $style_matches[1] = "normal";
+							$fontString .= "font-weight: ".$style_matches[1].";";
+							$fontString .= "font-style: ".$style_matches[2].";";
+						}else{
+							if($attr['style'] == 'regular') $attr['style'] = "normal";
+							$fontString .= "font-weight: ".$attr['style'].";";
+						}*/
+						$fontString .= "color: " . $attr['color'] . ";";
+						$line = $fontString;
+					} else {
+						$line = "//{$matches[1]} is empty\n";
+					}
+				} else {
+					$line = "//{$matches[1]} not found\n";
+				}
+			}
+			$parsedFileObj->fwrite( $line );
 		}
 	}
 }
@@ -168,11 +180,9 @@ function of_compile_less_on_save($option, $old_value, $value){
  */
 function of_is_admin_framework_page($hook){
 	$menu = Waboot_Options_Framework_Admin::menu_settings();
-
 	if ( $hook == 'waboot_page_' . $menu['old_menu_slug'] || $hook == 'toplevel_page_' . $menu['menu_slug']) {
 		return true;
 	}
-
 	return false;
 }
 
