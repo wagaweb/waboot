be8d91350f74597c3f12bd41c45d99eef2b1ce8f
LostCore <lostcore@gmail.com>
2015-01-20 11:18:24 +0100

    Ora il componente colobox permette di specificare i selettori a cui applicare lo script

--- components/colorbox/colorbox-custom.js
+++ components/colorbox/colorbox-custom.js
@@ -1,11 +1,36 @@
 jQuery(document).ready(function($){
-  $('a.img-colorbox').colorbox({
-	  maxWidth:"90%",
-	  maxHeight:"90%",
-	  rel: true,
-	  close: "",
-	  previous:"",
-	  next:"",
-	  current:"image {current} of {total}"
-  });
+    "use strict";
+    if(wabootCbox.elements !== false){
+        var elements = "";
+        var cboxProps = {
+            maxWidth: "90%",
+            maxHeight: "90%",
+            close: "",
+            previous: "",
+            next: "",
+            current: wabootCbox.current
+        };
+        switch(wabootCbox.elements){
+            case "galleries":
+                elements = ".gallery-item a";
+                cboxProps.rel = "gallery";
+                break;
+            case "custom":
+                if(wabootCbox.custom_elements !== false){
+                    elements = wabootCbox.custom_elements;
+                }else{
+                    elements = "a.img-colorbox";
+                }
+                break;
+            case "all-images":
+                elements = "a.img-colorbox";
+                cboxProps.rel = "cboximg";
+                break;
+            default:
+                elements = "a.img-colorbox";
+                cboxProps.rel = "cboximg";
+                break;
+        }
+        $(""+elements+"").colorbox(cboxProps);
+    }
 });
--- components/colorbox/colorbox.php
+++ components/colorbox/colorbox.php
@@ -10,9 +10,23 @@ Author URI: http://www.waga.it
 class ColorboxComponent extends Waboot_Component{
 
     public function scripts(){
-        wp_enqueue_script('component-colorbox',$this->directory_uri . '/jquery.colorbox-min.js',array('jquery'),false,false);
-        wp_enqueue_script('component-colorbox-custom',$this->directory_uri . '/colorbox-custom.js',array('jquery','component-colorbox'),false,false);
-    }  
+        wp_enqueue_script('component-colorbox',$this->directory_uri . '/jquery.colorbox-min.js',array('jquery'),false,true);
+        wp_register_script('component-colorbox-custom',$this->directory_uri . '/colorbox-custom.js',array('jquery','component-colorbox'),false,true);
+
+	    $cbox_elements = of_get_option($this->name.'_element');
+	    if($cbox_elements == "custom"){
+		    $cbox_custom_elements = of_get_option($this->name.'_custom_element');
+		    if($cbox_custom_elements == "") $cbox_custom_elements = false;
+	    }
+	    if($cbox_elements == "") $cbox_elements = false;
+
+	    wp_localize_script('component-colorbox-custom', 'wabootCbox', array(
+		    'elements' => $cbox_elements,
+		    'custom_elements' => isset($cbox_custom_elements) ? $cbox_custom_elements : false,
+		    'current' => __("image {current} of {total}","waboot")
+	    ) );
+	    wp_enqueue_script('component-colorbox-custom');
+    }
     
     public function styles(){
         wp_enqueue_style('component-colorbox-style',$this->directory_uri . '/colorbox.css');
@@ -30,6 +44,24 @@ class ColorboxComponent extends Waboot_Component{
 	            'col-sm-3' => 'col-sm-3'
 	        )
         );
+		$options[] = array(
+			'name' => __('Colorbox elements', 'waboot'),
+			'id' => $this->name.'_element',
+			'desc' => __('Select the type of elements on which to apply colorbox', 'waboot'),
+			'type' => 'radio',
+			'options' => array(
+				'all-images' => __("All images","waboot"),
+				'galleries' => __("Galleries only","waboot"),
+				'custom' => __("Custom selector","waboot"),
+			),
+			'std' => 'all-images'
+		);
+		$options[] = array(
+			'name' => __( 'Custom element', 'waboot' ),
+			'id'   => $this->name.'_custom_element',
+			'desc' => __( 'Enter a custom selector for colobox', 'waboot' ),
+			'type' => 'text'
+		);
         return $options;
     }
 }
