d3796073c62a5adf2adf496c1aa3293a47ad6efb
LostCore <lostcore@gmail.com>
2015-01-21 15:47:55 +0100

    - Bug fix unassigned var i

--- components/timeline/timeline.js
+++ components/timeline/timeline.js
@@ -1,12 +1,13 @@
 jQuery(document).ready(function($){
     "use strict";
 
-    $ ('ul.timeline li:odd').addClass('timeline-inverted');
+    //$ ('ul.timeline li:odd').addClass('timeline-inverted');
 
     var my_posts = $("[rel=tooltip]");
     var size = $(window).width();
 
     var the_post = "";
+    var i = 0;
     for(i=0;i<my_posts.length;i++){
         the_post = $(my_posts[i]);
         if(the_post.hasClass('invert') && size >=767 ){
