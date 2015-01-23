c6178461056fd46f729dd04d1b561aa16adf0db1
LostCore <lostcore@gmail.com>
2015-01-23 12:53:53 +0100

    - Spostate alcune funzioni che prima erano in utilities.php

--- wbf/admin/options-framework.php
+++ wbf/admin/options-framework.php
@@ -351,3 +351,59 @@ function of_add_default_key($values){
         'default' => $default
     );
 }
+
+/*
+ * IMPORT \ EXPORT FUNCTIONS (not used - we use the i\e functions into Waboot_Options_Framework_Admin)
+ */
+
+/**
+ * Replace the $old_prefix with $new_prefix in Theme Options id
+ * @param $old_prefix
+ * @param $new_prefix
+ * @since 0.1.0
+ */
+function prefix_theme_options($old_prefix, $new_prefix) {
+	$options_field = get_option('optionsframework');
+
+	if (!$options_field || empty($options_field)) return;
+
+	$options = get_option($options_field['id']);
+	$new_options = array();
+
+	if (!empty($options) && $options != false) {
+		foreach ($options as $k => $v) {
+			$new_k = preg_replace("|^" . $old_prefix . "_|", $new_prefix . "_", $k);
+			$new_options[$new_k] = $v;
+		}
+	} else {
+		return;
+	}
+
+	update_option($options_field['id'], $new_options);
+}
+
+/**
+ * Transfer theme options from a theme to another
+ * @param string $from_theme theme the name of the theme from which export
+ * @param (optional) null string $to_theme the name of the theme into which import (current theme if null)
+ * @totest
+ * @since 0.1.0
+ */
+function transfer_theme_options($from_theme, $to_theme = null) {
+	$from_theme_options = get_option($from_theme);
+	if (!isset($to_theme))
+		import_theme_options($from_theme_options);
+	else
+		update_option($to_theme, $from_theme_options);
+}
+
+/**
+ * Copy a theme options array into current theme options option. Old theme options will be replaced.
+ * @param array $exported_options
+ * @totest
+ * @since 0.1.0
+ */
+function import_theme_options($exported_options) {
+	$options_field = get_option('optionsframework');
+	update_option($options_field['id'], $exported_options);
+}
