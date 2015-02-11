afc5c61bd0e3d8e9b8ce3a3ac0f4212191b8a2f6
LostCore <lostcore@gmail.com>
2015-02-11 11:27:16 +0100

    Eliminato console.log

--- components/masonry/masonry-custom.js
+++ components/masonry/masonry-custom.js
@@ -2,7 +2,6 @@ jQuery(document).ready(function($) {
 	// Initialize Set options
     var $container = $('.blog-masonry');
     $container.imagesLoaded(function(){
-        console.log("Images loaded!");
         $container.masonry({
             // gutter: 20,
             itemSelector: '.item'
