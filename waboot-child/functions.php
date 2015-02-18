612300787f87c3c32474bda79f947fd1aee75dec
LostCore <lostcore@gmail.com>
2015-02-18 18:08:54 +0100

    Aggiunto child nel tema

---
+++ waboot-child/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+/*-----------------------------------------------------------------------------------*/
+/* Start Custom Functions - Please refrain from editing this section */
+/*-----------------------------------------------------------------------------------*/
+
+/**
+ * Make theme available for translation
+ * Translations can be filed in the /languages/ directory
+ * If you're building a theme based on waboot, use a find and replace
+ * to change 'waboot' to the name of your theme in all the template files
+ */
+load_theme_textdomain( 'waboot-child', get_stylesheet_directory() . '/languages' );
+
+
+
+
+/*-----------------------------------------------------------------------------------*/
+/* Don't add any code below here or the sky will fall down */
+/*-----------------------------------------------------------------------------------*/
