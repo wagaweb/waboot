390173a4a95679430156f3e2254e182f7bf27f4b
LostCore <lostcore@gmail.com>
2015-02-19 12:00:12 +0100

    Nuovo autoloader generale per il plugin framework

---
+++ wbf/includes/waboot-plugin/wbf-plugin-autoloader.php
@@ -0,0 +1,31 @@
+<?php
+
+spl_autoload_register( "wbf_plugin_autoload" );
+function wbf_plugin_autoload( $class ) {
+	$wbf_path = get_option( "wbf_path" );
+	if ( $wbf_path ) {
+		$plugin_main_class_dir = $wbf_path . "/includes/waboot-plugin";
+
+		if ( preg_match( "/^Waboot_Plugin|Template_/", $class ) ) {
+			$filename = "class-" . strtolower( preg_replace( "/_/", "-", $class ) ) . ".php";
+			if ( is_file( $plugin_main_class_dir . "/" . $filename ) ) {
+				require_once( $plugin_main_class_dir . "/" . $filename );
+			}
+		}
+
+		switch($class){
+			case "WBF\includes\Plugin_Update_Checker":
+				require_once($wbf_path . "/includes/plugin-update-checker.php");
+				break;
+			case "PluginUpdateChecker":
+			case "PluginUpdate":
+			case "PluginInfo":
+			case "PluginUpdateChecker_1_6":
+			case "PluginInfo_1_6":
+			case "PluginUpdate_1_6":
+			case "PucFactory":
+				require_once($wbf_path . "/vendor/plugin-updates/plugin-update-checker.php");
+				break;
+		}
+	}
+}
