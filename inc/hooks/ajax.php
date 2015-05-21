7f5bf899debc6944c346c2fb2e4a4796d4922d7e
LostCore <lostcore@gmail.com>
2015-05-21 11:26:38 +0200

    Ora la mail viene salvata sia che sia stata inviata con successo che no, e lo stato viene salvato nella tabella

--- inc/hooks/ajax.php
+++ inc/hooks/ajax.php
@@ -5,19 +5,22 @@ add_action('wp_ajax_nopriv_wbft_send_contact_email', 'wbft_send_contact_email');
 
 function wbft_send_contact_email(){
 	$save_mail = function($data){
-		return true;
+		global $wpdb;
+		$result = $wpdb->insert($wpdb->prefix . "wb_mails",$data);
+		return $result;
 	};
 
+	//Looking for $_POST params setter? Looking in /sources/js/controllers/contactForm.js
 	$mail_data = apply_filters("wbft/contact_form/mail/data",$_POST);
 	$save_mail_data = apply_filters("wbft/contact_form/mail/save/data",$_POST);
 
 	if(wp_mail($mail_data['to'],$mail_data['subject'],$mail_data['message'],$mail_data['headers'])){
-		if($save_mail($save_mail_data)){ //todo: fare il salvataggio della mail nel DB
-			wbft_ajax_out(2);
-		}else{
-			wbft_ajax_out(1);
-		}
+		$save_mail_data['status'] = 1;
+		$save_mail($save_mail_data);
+		wbft_ajax_out(2);
 	}else{
+		$save_mail_data['status'] = 0;
+		$save_mail($save_mail_data);
 		wbft_ajax_out(0);
 	}
 }
