1e60608460f86370e6be7248bd31ac2f75e4ca6a
LostCore <lostcore@gmail.com>
2015-06-12 14:05:56 +0200

    fix funzione waboot_get_first_video

--- templates/parts/content-video.php
+++ templates/parts/content-video.php
@@ -7,7 +7,7 @@
 <article role="article" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <?php do_action( 'waboot_entry_header' ); ?>
 	<div class="entry-content">
-		<?php $first_video = et_get_first_video(); ?>
+		<?php $first_video = waboot_get_first_video(); ?>
 		<?php if ( $first_video ) : ?>
 			<div class="wb-video-container">
 				<?php echo $first_video; ?>
