3712d316b691d46b7bcad69eba821f3fa7c00b5d
LostCore <lostcore@gmail.com>
2015-08-22 20:39:43 +0200

    Applicato flush() dopo l'<head>

--- inc/hooks.php
+++ inc/hooks.php
@@ -5,6 +5,16 @@ require_once("hooks/entry-footer.php");
 require_once("hooks/layout.php");
 require_once("hooks/ajax.php");
 
+if(!function_exists("wbft_header_flush")):
+	/**
+	 * Flush(); after the header: https://developer.yahoo.com/performance/rules.html
+	 */
+	function wbft_header_flush(){
+		flush();
+	}
+	add_action( 'waboot_head_after', 'wbft_header_flush', 0 );
+endif;
+
 if(!function_exists('waboot_do_site_title')):
     /**
      * Displays site title at top of page
