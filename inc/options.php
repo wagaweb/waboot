b0f7bb61937e4c3c99759e4d6d54a8d044068928
LostCore <lostcore@gmail.com>
2015-08-27 14:15:08 +0200

    Rimossa funzione deprecata "optionsframework_option_name"

--- inc/options.php
+++ inc/options.php
@@ -1,23 +1,6 @@
 <?php
 
 /**
- * A unique identifier is defined to store the options in the database and reference them from the theme.
- * By default it uses the theme name, in lowercase and without spaces, but this can be changed if needed.
- * If the identifier changes, it'll appear as if the options have been reset.
- *
- */
-function optionsframework_option_name() {
-
-    // This gets the theme name from the stylesheet
-    $themename = get_option( 'stylesheet' );
-    $themename = preg_replace("/\W/", "_", strtolower($themename) );
-
-    $optionsframework_settings = get_option('optionsframework');
-    $optionsframework_settings['id'] = $themename;
-    update_option('optionsframework', $optionsframework_settings);
-}
-
-/**
  * Defines an array of options that will be used to generate the settings page and be saved in the database.
  * When creating the "id" fields, make sure to use all lowercase and no spaces.
  *
