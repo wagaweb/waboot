f6f34fb4d54c34f2cd40b4f21b3d4189260f7022
LostCore <lostcore@gmail.com>
2015-08-31 11:59:45 +0200

    Eliminato il comportamento per il quale se l'array delle opzioni risultava vuoto durante l'inizializzazione, veniva riempito con le opzioni di default. Ora non è più necessario.

--- wbf/modules/options/bootstrap.php
+++ wbf/modules/options/bootstrap.php
@@ -70,12 +70,12 @@ function optionsframework_init() {
     $options_framework_waboot_gfont_selector->init();
 
 	//If there are no options for current theme, then add the defaults
-	$values = Framework::get_options_values();
+	/*$values = Framework::get_options_values();
 	if(!$values || empty($values)){
-		$defaults = $options_framework_admin->get_default_values();
+		$defaults = Framework::get_default_values();
 		of_options_save(Framework::get_options_root_id(),[],$defaults);
 		Framework::update_theme_options($defaults);
-	}
+	}*/
 }
 
 /**
