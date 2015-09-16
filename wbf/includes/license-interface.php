3ad2082f8fc8f1880224ab9edffb43371e881382
LostCore <lostcore@gmail.com>
2015-09-16 13:35:38 +0200

    Commentate le funzioni dell'interfaccia

--- wbf/includes/license-interface.php
+++ wbf/includes/license-interface.php
@@ -3,8 +3,26 @@
 namespace WBF\includes;
 
 interface License_Interface {
+	/**
+	 * Checks the license code under the proprietary algorithm
+	 * @param $args
+	 * @return mixed
+	 */
 	function check_license($args);
+	/**
+	 * Sanitize the license code
+	 * @param $license_code
+	 * @return string
+	 */
 	function sanitize_license($license_code);
+	/**
+	 * Get the license status. Must return "Active" if the provided license code is valid.
+	 * @return string
+	 */
 	function get_license_status();
+	/**
+	 * Print out the license status
+	 * @return string
+	 */
 	function print_license_status();
 }
