662a10519a9a6c5fe8fa9a7a89245712987aedd9
LostCore <lostcore@gmail.com>
2015-09-23 18:37:10 +0200

    Eliminato WBF!

--- wbf/includes/waboot-plugin/class-waboot-template-plugin.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$wbf_path = get_option( "wbf_path" );
-require_once $wbf_path."/includes/pluginsframework/autoloader.php";
-
-class Waboot_Template_Plugin extends WBF\includes\pluginsframework\TemplatePlugin {}
--- wbf/sources/js/admin/views/acf-fields.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    multipleFileUpload: require("./acf-fields/multiple-file-upload")
-};
