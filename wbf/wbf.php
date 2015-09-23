b68353dbb1d2afa6623e7f9cb4e4985ef027ad28
LostCore <lostcore@gmail.com>
2015-09-23 12:53:24 +0200

    Aggiornato numero versione

--- wbf/wbf.php
+++ wbf/wbf.php
@@ -19,7 +19,7 @@ include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
 class WBF {
 
-	const version = "0.13.2";
+	const version = "0.13.3";
 
 	static function startup($args = []){
 		$args = wp_parse_args($args,[
