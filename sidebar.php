e6f7480ec341042d2f01b0fa65c3351d3ed9770c
LostCore <lostcore@gmail.com>
2015-09-29 21:16:18 +0200

    Aggiunte nuovi hook alla sidebar

--- sidebar.php
+++ sidebar.php
@@ -9,7 +9,9 @@
 <?php if ( waboot_get_body_layout() != "full-width" ) : ?>
 	<div id="primary" class="<?php echo apply_filters( 'waboot_primary_container_class', 'col-sm-4' ); ?>">
 		<div id="sidebar-primary" class="<?php echo apply_filters( 'waboot_sidebar_container_class', 'widget-area' ); ?>" role="complementary">
+			<?php do_action("waboot/sidebar/primary/widgets/before"); ?>
 			<?php dynamic_sidebar( 'sidebar-1' ); ?>
+			<?php do_action("waboot/sidebar/primary/widgets/after"); ?>
 		</div><!-- #sidebar -->
 	</div><!-- #secondary -->
 <?php endif; ?>
@@ -17,7 +19,9 @@
 <?php if ( waboot_body_layout_has_two_sidebars() ) : ?>
 	<div id="secondary" class="<?php echo apply_filters( 'waboot_secondary_container_class', 'col-sm-4' ); ?>">
 		<div id="sidebar-secondary" class="<?php echo apply_filters( 'waboot_sidebar_container_class', 'widget-area' ); ?>" role="complementary">
+			<?php do_action("waboot/sidebar/secondary/widgets/before"); ?>
 			<?php dynamic_sidebar( 'sidebar-2' ); ?>
+			<?php do_action("waboot/sidebar/secondary/widgets/after"); ?>
 		</div><!-- #sidebar -->
 	</div><!-- #secondary -->
 <?php endif; ?>
