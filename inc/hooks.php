df9b6147450c2a09e5b053d887ee5fd9a07d9bba
LostCore <lostcore@gmail.com>
2015-09-29 21:12:00 +0200

    Eliminati hook ridondanti

--- inc/hooks.php
+++ inc/hooks.php
@@ -15,52 +15,6 @@ if(!function_exists("wbft_header_flush")):
 	add_action( 'waboot_head_after', 'wbft_header_flush', 0 );
 endif;
 
-if(!function_exists('waboot_do_site_title')):
-    /**
-     * Displays site title at top of page
-     * @since 0.1.0
-     */
-    function waboot_do_site_title() {
-        // Use H1
-        $element = 'h1';
-        // Title content that goes inside wrapper
-        $site_name = sprintf( '<a href="%s" title="%s" rel="home">%s</a>', trailingslashit( home_url() ), esc_attr( get_bloginfo( 'name' ) ), get_bloginfo( 'name' ) );
-        // Put it all together
-        $title = '<' . $element . ' id="site-title" class="site-title">' . $site_name . '</' . $element .'>';
-        // Echo the title
-        echo apply_filters( 'waboot_site_title_content', $title );
-    }
-    add_action( 'waboot_site_title', 'waboot_do_site_title' );
-endif;
-
-if(!function_exists('waboot_do_site_description')):
-    /**
-     * Displays site description at top of page
-     * @since 0.1.0
-     */
-    function waboot_do_site_description() {
-	    if(!of_get_option("show_site_description",0)) return;
-        // Use H2
-        $element = 'h2';
-        // Put it all together
-        $description = '<' . $element . ' id="site-description" class="site-description">' . esc_html( get_bloginfo( 'description' ) ) . '</' . $element . '>';
-        // Echo the description
-        echo apply_filters( 'waboot_site_description_content', $description );
-    }
-    add_action( 'waboot_site_description', 'waboot_do_site_description' );
-endif;
-
-if(!function_exists('waboot_do_archive_page_title')):
-    /**
-     * Display page title on archive pages
-     * @since 0.1.0
-     */
-    function waboot_do_archive_page_title($prefix = "",$suffix = "") {
-	    waboot_archive_page_title($prefix,$suffix,true);
-    }
-    add_action( 'waboot_archive_page_title', 'waboot_do_archive_page_title', 10, 2 );
-endif;
-
 if(!function_exists('waboot_behaviors_cpts_blacklist')):
     /**
      * Puts some custom post types into blacklist (in these post types the behavior will never be displayed)
