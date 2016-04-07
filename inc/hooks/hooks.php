cd73e1876a1adfda35f6a07da6db1665935ac794
LostCore <lostcore@gmail.com>
2016-04-07 11:45:06 +0200

    Strutturazione p4

--- inc/hooks/hooks.php
+++ inc/hooks/hooks.php
@@ -9,3 +9,10 @@ function add_header_metas(){
 	get_template_part("templates/parts/meta");
 }
 add_action("waboot/head/start",__NAMESPACE__."add_header_meta");
+
+function add_apple_touch_icon(){
+	?>
+	<link rel="apple-touch-icon" href="<?php apply_filters("waboot/assets/apple-touch-icon-path","apple-touch-icon.png"); ?>">
+	<?php
+}
+add_action("waboot/head/meta",__NAMESPACE__."add_apple_touch_icon");
--- templates/parts/meta.php
+++ templates/parts/meta.php
@@ -1,6 +1,6 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <!-- [if IE]> <meta http-equiv="X-UA-Compatible" content="IE=Edge"/> <! [endif]-->
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php do_action("waboot/head/meta"); ?>
