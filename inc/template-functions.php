3a0002fcd7ce1ee380d6678002eb17042b845c33
LostCore <lostcore@gmail.com>
2016-04-08 15:27:17 +0200

    Compatibilità in assenza di WBF

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -9,7 +9,15 @@ function Waboot(){
 	return \Waboot\Theme::getInstance();
 }
 
-if(!function_exists("wb_get_option")):
+if(!function_exists("wb_get_option") && function_exists('\WBF\modules\options\of_get_option')):
+	/**
+	 * Wrapper for \WBF\modules\options\of_get_option
+	 *
+	 * @param $name
+	 * @param bool $default
+	 *
+	 * @return bool|mixed
+	 */
 	function wb_get_option($name, $default = false){
 		if(class_exists("WBF")){
 			return \WBF\modules\options\of_get_option($name,$default);
