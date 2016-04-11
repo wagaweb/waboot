cbea9683bbf30ffb9768f0392353d3694cb8fe34
lostcore@gmail.com <123qw45>
2016-04-11 12:31:33 +0200

    Fix namespace

--- inc/hooks/hooks.php
+++ inc/hooks/hooks.php
@@ -8,7 +8,7 @@ namespace Waboot\hooks;
 function add_header_metas(){
 	get_template_part("templates/parts/meta");
 }
-add_action("waboot/head/start",__NAMESPACE__."add_header_meta");
+add_action("waboot/head/start",__NAMESPACE__."\\add_header_metas");
 
 /**
  * Adds apple touch init to the document head meta
@@ -18,4 +18,4 @@ function add_apple_touch_icon(){
 	<link rel="apple-touch-icon" href="<?php apply_filters("waboot/assets/apple-touch-icon-path","apple-touch-icon.png"); ?>">
 	<?php
 }
-add_action("waboot/head/meta",__NAMESPACE__."add_apple_touch_icon");
+add_action("waboot/head/meta",__NAMESPACE__."\\add_apple_touch_icon");
