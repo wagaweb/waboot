0c02057386373e2f7b13072c0e8292eb1d2564f1
LostCore <lostcore@gmail.com>
2016-04-15 18:33:42 +0200

    Si prosegue coi componenti

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
-    <div id="main-wrap">
+    <div id="main-wrapper">
 	    <?php if(function_exists("Waboot")): ?>
         <?php Waboot()->layout->render_zone("aside-primary"); ?>
         <?php Waboot()->layout->render_zone("main"); ?>
         <?php Waboot()->layout->render_zone("aside-secondary"); ?>
 		<?php endif; ?>
-    </div><!-- #main-wrap -->
+    </div><!-- #main-wrapper -->
 <?php get_footer(); ?>
