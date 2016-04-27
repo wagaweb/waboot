74ab6ec43aafa298c9b52735f8be9d73e9c425fb
lostcore@gmail.com <123qw45>
2016-04-27 13:46:24 +0200

    Aggiunto phpunit p2

---
+++ tests/SimpleTest.php
@@ -0,0 +1,9 @@
+<?php
+class SimpleTest extends PHPUnit_Framework_TestCase{
+	public function testSimple(){
+		$a = true;
+		$b = true;
+		// Assert
+		$this->assertEquals($a, $b);
+	}
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+$current_path = dirname(__FILE__);
+
+require_once dirname($current_path)."/vendor/autoload.php";
