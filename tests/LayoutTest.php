194c2dea916b62e1b15787c11ece26236a978da6
lostcore@gmail.com <123qw45>
2016-04-27 18:49:24 +0200

    Test per Layout

--- tests/LayoutTest.php
+++ tests/LayoutTest.php
@@ -3,8 +3,12 @@
 namespace tests;
 
 use Waboot\Layout;
+use WBF\includes\mvc\HTMLView;
 
 class LayoutTest extends \PHPUnit_Framework_TestCase{
+	/**
+	 * @var Layout
+	 */
 	var $layout;
 
 	public function setUp() {
@@ -78,6 +82,38 @@ class LayoutTest extends \PHPUnit_Framework_TestCase{
 	 */
 	public function testLayoutExists(){
 		$this->assertInstanceOf("Waboot\\Layout",$this->layout);
+	}
+
+	/**
+	 * Testing the creare zone mechanism
+	 */
+	public function testCreateZones(){
+		\WP_Mock::wpFunction('get_template_directory',[
+			'args' => [],
+			'return' => WBTEST_WP_CONTENT_PATH."/themes/waboot"
+		]);
+
+		\WP_Mock::wpFunction('get_stylesheet_directory',[
+			'args' => [],
+			'return' => WBTEST_WP_CONTENT_PATH."/themes/waboot"
+		]);
+
+		\WP_Mock::wpFunction('wp_parse_args', [
+			'args' => [
+				['always_load' => true],
+				['always_load' => false]
+			],
+			'return' => [
+				['always_load' => true ]
+			]
+		]);
+		
+		$this->layout->create_zone("header",new HTMLView("templates/header.php"),["always_load"=>true]);
+
+		$zones = $this->layout->getZones();
+		$this->assertEquals(true,is_array($zones)); //The zones is an array?
+		$this->assertEquals(1,count($zones)); //The zones as 1 element?
+		$this->assertArrayHasKey("header",$zones); //We have our zone?
 	}
 	
 	public function tearDown() {
