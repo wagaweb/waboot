f1611cfe946c95dd798cc86d46295e4a9d93edd6
LostCore <lostcore@gmail.com>
2016-04-29 10:38:54 +0200

    Loading di WP nei test

--- tests/bootstrap-wp.php
+++ tests/bootstrap-wp.php
@@ -4,10 +4,9 @@ error_reporting( E_ALL & ~E_DEPRECATED & ~E_STRICT );
 define("WBTEST_CURRENT_PATH",dirname(__FILE__));
 define("WBTEST_WP_CONTENT_PATH",dirname(dirname(dirname(WBTEST_CURRENT_PATH))));
 define("WBTEST_WORDPRESS_PATH",dirname(dirname(dirname(dirname(WBTEST_CURRENT_PATH)))));
+define("WBTEST_CONFIG_PATH",WBTEST_CURRENT_PATH.'/configs/wp-tests-config.php');
 
 require_once dirname(WBTEST_CURRENT_PATH)."/vendor/autoload.php";
-
-$config_file_path = WBTEST_CURRENT_PATH . '/configs/wp-tests-config.php';
 
 /*
  * Globalize some WordPress variables, because PHPUnit loads this file inside a function
@@ -15,17 +14,24 @@ $config_file_path = WBTEST_CURRENT_PATH . '/configs/wp-tests-config.php';
  */
 global $wpdb, $current_site, $current_blog, $wp_rewrite, $shortcode_tags, $wp;
 
-if(!is_readable( $config_file_path)){
+//Load configs
+if(!is_readable( WBTEST_CONFIG_PATH)){
 	die( "ERROR: wp-tests-config.php is missing! Please use wp-tests-config-sample.php to create a config file.\n" );
 }
-require_once $config_file_path;
+require_once WBTEST_CONFIG_PATH;
+
+//Load utility functions
+require_once WBTEST_CURRENT_PATH . '/includes/functions.php';
+
+global $table_prefix;
+
+define('WP_TESTS_TABLE_PREFIX', $table_prefix);
 
 if(!defined('WP_TESTS_FORCE_KNOWN_BUGS')){
 	define('WP_TESTS_FORCE_KNOWN_BUGS',false);
 }
 
-// Cron tries to make an HTTP request to the blog, which always fails, because tests are run in CLI mode only
-define( 'DISABLE_WP_CRON', true );
+define( 'DISABLE_WP_CRON', true ); // Cron tries to make an HTTP request to the blog, which always fails, because tests are run in CLI mode only
 
 define('WP_MEMORY_LIMIT', -1);
 define('WP_MAX_MEMORY_LIMIT', -1);
@@ -34,5 +40,31 @@ $_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
 $_SERVER['HTTP_HOST'] = WP_TESTS_DOMAIN;
 $PHP_SELF = $GLOBALS['PHP_SELF'] = $_SERVER['PHP_SELF'] = '/index.php';
 
-// Load WordPress
+//Override the PHPMailer
+require_once( WBTEST_CURRENT_PATH . '/includes/mock-mailer.php' );
+$phpmailer = new MockPHPMailer();
+
+$GLOBALS['_wp_die_disabled'] = false;
+//Allow tests to override wp_die
+tests_add_filter( 'wp_die_handler', '_wp_die_handler_filter' );
+
+/*
+ * Preset WordPress options defined in bootstrap file.
+ * Used to activate themes, plugins, as well as other settings.
+ */
+if(isset($GLOBALS['wp_tests_options'])){
+	function wp_tests_options($value){
+		$key = substr( current_filter(), strlen( 'pre_option_' ));
+		return $GLOBALS['wp_tests_options'][$key];
+	}
+	foreach(array_keys($GLOBALS['wp_tests_options']) as $key){
+		tests_add_filter('pre_option_'.$key, 'wp_tests_options');
+	}
+}
+
+//Load WordPress
 require_once ABSPATH . '/wp-settings.php';
+
+//Load this strange class...
+require_once WBTEST_CURRENT_PATH . '/includes/WP_PHPUnit_Util_Getopt.php';
+new WP_PHPUnit_Util_Getopt( $_SERVER['argv'] );
--- tests/configs/wp-tests-config.php
+++ tests/configs/wp-tests-config.php
@@ -1,26 +1,29 @@
 <?php
 define( 'ABSPATH', WBTEST_WORDPRESS_PATH."/" );
+
 // Force known bugs: (previously -f)
 define( 'WP_TESTS_FORCE_KNOWN_BUGS', true );
+
 // Test with WordPress debug mode on (previously -d)
 define( 'WP_DEBUG', true );
 
 // ** MySQL settings ** //
 // WARNING WARNING WARNING!
 // wp-test will MIGHT DROP ALL TABLES in the database named below.
-define( 'DB_NAME', 'throwaway' ); 
+define( 'DB_NAME', 'waga_waboot' );
 define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'root' );
 define( 'DB_HOST', 'localhost' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
+$table_prefix  = 'wp_';   // Only numbers, letters, and underscores please!
+
 // ** Site settings ** //
-define( 'WP_TESTS_DOMAIN', 'localhost' );
-define( 'WP_TESTS_EMAIL', 'admin@example.org' );
-define( 'WP_TESTS_TITLE', 'Test Blog' );
+define( 'WP_TESTS_DOMAIN', 'waboot.dev' );
+define( 'WP_TESTS_EMAIL', 'dev@waga.it' );
+define( 'WP_TESTS_TITLE', 'Waboot' );
+
 define( 'WP_PHP_BINARY', 'php' );
 
 define ( 'WPLANG', '' );
-
-$table_prefix  = 'wp_';   // Only numbers, letters, and underscores please!
