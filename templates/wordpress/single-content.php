ab254edf61922864114188c8d22e56d986c27ddf
LostCore <lostcore@gmail.com>
2016-05-05 12:28:20 +0200

    Spaziature

--- templates/wordpress/single-content.php
+++ templates/wordpress/single-content.php
@@ -1,6 +1,6 @@
-<?php \Waboot\template_tags\post_navigation( 'nav-above' ); ?>
+<?php \Waboot\template_tags\post_navigation('nav-above'); ?>
 <?php get_template_part('templates/post-formats/content', get_post_format()); ?>
-<?php \Waboot\template_tags\post_navigation( 'nav-below' ); ?>
+<?php \Waboot\template_tags\post_navigation('nav-below'); ?>
 <?php
 if(comments_open() || '0' != get_comments_number()){
 	comments_template('/comments.php',true);
