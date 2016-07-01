159a3aa7378b1c84ba67ead34940bf5841e3be6f
LostCore <lostcore@gmail.com>
2016-07-01 13:01:34 +0200

    - Eliminato contact form - Refactoring generale

--- assets/src/js/controllers/dashboard.js
+++ assets/src/js/controllers/dashboard.js
@@ -8,27 +8,8 @@ export default class extends Backbone.Model{
     }
     do_stuff(){
         "use strict";
-        var $mailtable = $("#waboot-received-mails-view"),
-            $recent_posts_widget_pt_selector = $("#widgets-right [data-wbrw-post-type-selector]");
-        /**
-         * Init received mails viewer
-         */
-        if($mailtable.length > 0){
-            var MailListView = require("../views/mailList.js"),
-                MailListModel = require("./mailList.js"),
-                MailWindow = new MailListView({
-                    model: new MailListModel({
-                        emails_data: (function(){
-                            if(!_.isUndefined(wbData.contactForm.mails)){
-                                return jQuery.parseJSON(wbData.contactForm.mails);
-                            }else{
-                                return [];
-                            }
-                        })()
-                    }),
-                    el: $mailtable
-                });
-        }
+        var $recent_posts_widget_pt_selector = $("#widgets-right [data-wbrw-post-type-selector]");
+        
         /**
          * RECENT POST WIDGET
          */
--- assets/src/js/controllers/frontend.js
+++ assets/src/js/controllers/frontend.js
@@ -5,23 +5,15 @@ import $ from "jquery";
 export default class extends Backbone.Model{
     initialize() {
         "use strict";
-        this.do_stuff();
+        this.on_ready();
     }
-    do_stuff(){
+    on_ready(){
         "use strict";
         /*
          * Bootstrapping html elements
          */
-        $('input[type=text]').addClass('form-control');
-        $('input[type=select]').addClass('form-control');
-        $('textarea').addClass('form-control');
-        $('select').addClass('form-control');
-        $('input#submit').addClass('btn btn-primary');
-        $('.gform_button').addClass('btn btn-primary btn-lg').removeClass('gform_button button');
-        $('.validation_error').addClass('alert alert-danger').removeClass('validation_error');
-        $('.gform_confirmation_wrapper').addClass('alert alert-success').removeClass('gform_confirmation_wrapper');
-        // Tables
-        $('table').addClass('table');
+        this.apply_bootstrap();
+        
         /*
          * These will make any element that has data-wbShow\wbHide="<selector>" act has visibily toggle for <selector>
          */
@@ -42,70 +34,110 @@ export default class extends Backbone.Model{
                 itemToShow.hide();
             }
         });
-        /*
-         * INIT CONTACT FORM
-         */
-        var ContactFormView = require("../views/contactForm.js"),
-            ContactFormModel = require("./contactForm.js"),
-            $contactForm = $("[data-contactForm]");
-        //Init search windows
-        if ($contactForm.length > 0) {
-            let contactWindow = new ContactFormView({
-                model: new ContactFormModel(),
-                el: $contactForm
-            });
-        }
+        
         /*
          * MOBILE ACTIONS
          */
         if (wbData.isMobile){
-            //swipe = require("TouchSwipe");
-            //http://getbootstrap.com/getting-started/#support
-            if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-                let msViewportStyle = document.createElement('style');
-                msViewportStyle.appendChild(
-                    document.createTextNode(
-                        '@-ms-viewport{width:auto!important}'
-                    )
-                );
-                document.querySelector('head').appendChild(msViewportStyle);
-            }
-            FastClick.attach(document.body);
-            /*$("body").swipe({
-                swipeRight: function(event, direction, distance, duration, fingerCount) {
-                    if ($(".navbar-mobile-collapse").css('right') == '0px') {
-                        $('button.navbar-toggle').trigger('click');
-                    }
-                },
-                swipeLeft: function(event, direction, distance, duration, fingerCount) {
-                    if ($(".navbar-mobile-collapse").css('right') == '0px') {
-                        $('button.navbar-toggle').trigger('click');
-                    }
-                }
-            });*/
-            //Disable for Metaslider
-            $(".metaslider").addClass("noSwipe");
+            this.do_mobile_actions();
         }
+        
         /*
          * WOOCOMMERCE
          */
-        $('.woocommerce a.button').addClass('btn');
-        $('.woocommerce a.add_to_cart_button').addClass('btn-success');
-        $('.woocommerce a.button').removeClass('button');
-        $('.woocommerce table.cart').addClass('table-striped');
-        $('.woocommerce table.cart').removeClass('shop_table');
-        $('.woocommerce table.cart td.actions input.button').addClass('btn');
-        $('.woocommerce table.cart td.actions input.button').addClass('btn-default');
-        $('.woocommerce table.cart td.actions input.button').removeClass('button');
+        if(typeof wbData !== "undefined" && wbData.has_woocommerce){
+            this.do_wc_actions();
+        }
+        
         //Enabling tab navigation
-        /*$("[role=tablist] li a").each(function() {
-         var self = this;
-         $(this).on("click", function(e) {
-         e.preventDefault();
-         self.tab("show");
-         });
-         });*/
+        /*
+        $("[role=tablist] li a").each(function() {
+            var self = this;
+            $(this).on("click", function(e) {
+                e.preventDefault();
+                self.tab("show");
+            });
+        });
+        */
+        
         $(".nav-tabs li:first-child").addClass("active");
         $(".tab-content .tab-pane:first-child").addClass("active");
+    }
+
+    /**
+     * Bootstrapping html elements
+     */
+    apply_bootstrap(){
+        "use strict";
+        $('input[type=text]').addClass('form-control');
+        $('input[type=select]').addClass('form-control');
+        $('textarea').addClass('form-control');
+        $('select').addClass('form-control');
+        $('input#submit').addClass('btn btn-primary');
+        $('.gform_button').addClass('btn btn-primary btn-lg').removeClass('gform_button button');
+        $('.validation_error').addClass('alert alert-danger').removeClass('validation_error');
+        $('.gform_confirmation_wrapper').addClass('alert alert-success').removeClass('gform_confirmation_wrapper');
+        // Tables
+        $('table').addClass('table');        
+    }
+    
+    /**
+     * Performs mobile oriented actions
+     */
+    do_mobile_actions(){
+        "use strict";
+        let $metaslider = $(".metaslider");
+        //let swipe = require("TouchSwipe");
+        
+        //http://getbootstrap.com/getting-started/#support
+        if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+            let msViewportStyle = document.createElement('style');
+            msViewportStyle.appendChild(
+                document.createTextNode(
+                    '@-ms-viewport{width:auto!important}'
+                )
+            );
+            document.querySelector('head').appendChild(msViewportStyle);
+        }
+        
+        if(typeof FastClick !== "undefined"){
+            FastClick.attach(document.body);   
+        }
+        
+        /*
+        $("body").swipe({
+            swipeRight: function(event, direction, distance, duration, fingerCount) {
+                if ($(".navbar-mobile-collapse").css('right') == '0px') {
+                    $('button.navbar-toggle').trigger('click');
+                }
+            },
+            swipeLeft: function(event, direction, distance, duration, fingerCount) {
+                if ($(".navbar-mobile-collapse").css('right') == '0px') {
+                    $('button.navbar-toggle').trigger('click');
+                }
+            }
+         });
+         */
+        
+        //Disable for Metaslider
+        if(typeof $metaslider !== "undefined"){
+            $metaslider.addClass("noSwipe");
+        }
+    }
+    
+    /**
+     * Performs some adjustment for WooCommerce
+     */
+    do_wc_actions(){
+        "use strict";
+        let $wc_container = $('.woocommerce'),
+            $wc_cart_table = $wc_container.find("table.cart");
+        
+        $wc_container.find('a.button').addClass('btn');
+        $wc_container.find('a.add_to_cart_button').addClass('btn-success');
+        $wc_container.find('a.button').removeClass('button');
+        
+        $wc_cart_table.addClass('table-striped');
+        $wc_cart_table.find('td.actions input.button').addClass('btn').addClass('btn-default').removeClass('button');
     }
 }
