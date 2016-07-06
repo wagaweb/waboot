9e07ca05364fcd1147c9549e0cad35f2ecef10a9
lostcore@gmail.com <123qw45>
2016-07-06 14:59:10 +0200

    Implementazioni nuovi stili

--- inc/hooks/stylesheets.php
+++ inc/hooks/stylesheets.php
@@ -18,12 +18,34 @@ add_action( 'wp_enqueue_scripts', __NAMESPACE__.'\\register_fa' );
  * Loads frontend styles
  */
 function theme_styles() {
-	//Main style
-	$file = get_stylesheet_directory()."/assets/dist/css/waboot.css";
+	$file = get_stylesheet_directory()."/assets/dist/css/waboot.min.css";
 	if(is_readable($file)){
 		$version = filemtime($file);
-		wp_register_style('waboot-style', get_stylesheet_directory_uri()."/assets/dist/css/waboot.css", [], $version, 'all' ); //waboot.css
+		wp_register_style('waboot-style', get_stylesheet_directory_uri()."/assets/dist/css/waboot.min.css", [], $version, 'all' );
 	}
-	wp_enqueue_style('core-style', get_stylesheet_uri(), ['waboot-style'], false, 'all' ); //style.css
+	wp_enqueue_style('core-style', get_stylesheet_uri(), ['waboot-style'], false, 'all' ); //enqueue style.css
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__.'\\theme_styles' );
+
+/**
+ * Loads backend styles
+ */
+function admin_styles(){
+	$file = get_stylesheet_directory()."/assets/dist/css/waboot-admin.min.css";
+	if(is_readable($file)){
+		$version = filemtime($file);
+		wp_register_style('waboot-admin-style', get_stylesheet_directory_uri()."/assets/dist/css/waboot-admin.min.css", [], $version, 'all' );
+		wp_enqueue_style('waboot-admin-style');
+	}
+}
+add_action( 'admin_enqueue_scripts', __NAMESPACE__.'\\admin_styles' );
+
+/**
+ * Apply custom stylesheet to the wordpress visual editor.
+ *
+ * @uses add_editor_style()
+ */
+function editor_style() {
+	add_editor_style(get_stylesheet_directory_uri()."/assets/dist/css/waboot-admin-tinymce.min.css");
+}
+add_action('init', __NAMESPACE__.'\\editor_style');
