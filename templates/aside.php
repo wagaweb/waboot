94e3846db006b3e9811204348908645e02ca679c
LostCore <lostcore@gmail.com>
2016-07-07 17:25:33 +0200

    Eliminato testo dummy

--- templates/aside.php
+++ templates/aside.php
@@ -6,7 +6,6 @@
 <?php $vars = \Waboot\functions\get_aside_template_vars($name); ?>
 <aside id="<?php echo $name ?>" class="<?php echo $vars['classes']; ?>" role="complementary">
 	<div class="<?php echo $vars['container_classes'] ?>">
-		Waboot Aside
 		<?php Waboot()->layout->do_zone_action($name); ?>
 		<?php do_action("waboot/aside"); ?>
 	</div>
