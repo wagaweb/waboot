9a080255b411ea9171de608820d6310af2549b88
LostCore <lostcore@gmail.com>
2016-07-08 10:16:06 +0200

    Aggiunti i behaviors

--- inc/hooks/options.php
+++ inc/hooks/options.php
@@ -4,12 +4,13 @@ namespace Waboot\hooks\options;
 
 use WBF\modules\options\Organizer;
 
-add_filter('wbf/modules/options/available','\Waboot\hooks\options\register');
+add_filter('wbf/modules/options/available', __NAMESPACE__.'\\register_options');
+add_filter("wbf/modules/behaviors/available", __NAMESPACE__."\\register_behaviors");
 
 /**
  * Register standard theme options
  */
-function register(){
+function register_options(){
 	$orgzr = Organizer::getInstance();
 
 	$orgzr->set_group("std_options");
@@ -344,4 +345,251 @@ function register(){
 
 	$orgzr->reset_group();
 	$orgzr->reset_section();
+}
+
+/**
+ * Register Waboot Behaviors.
+ *
+ * Behaviors are registered as options by Behaviors WBF Module @ register_behaviors_as_theme_options()
+ *
+ * @param array $behaviors
+ *
+ * @return array
+ */
+function register_behaviors($behaviors){
+
+	$imagepath = get_template_directory_uri() . '/assets/images/options';
+
+	$behaviors[] = array(
+		"name" => "show-title",
+		"title" => __("Display page title","waboot"),
+		"desc" => __("Default rendering value for page title","waboot"),
+		"options" => array(
+			array(
+				"name" => __("Yes"),
+				"value" => 1
+			),
+			array(
+				"name" => __("No"),
+				"value" => 0
+			)
+		),
+		"type" => "select",
+		"default" => 1,
+		"valid" => array("page","post","-{blog}","{cpt}","-slideshow","-{ctag:waboot_woocommerce_is_shop}")
+	);
+
+	$behaviors[] = array(
+		"name" => "title-position",
+		"title" => __("Title position","waboot"),
+		"desc" => __("Default title positioning in pages","waboot"),
+		"type" => "select",
+		"options" => array(
+			array(
+				"name" => __("Above primary","waboot"),
+				"value" => "top"
+			),
+			array(
+				"name" => __("Below primary","waboot"),
+				"value" => "bottom"
+			)
+		),
+		"default" => "top",
+		"valid" => array("page","post","-{blog}","{cpt}","-slideshow","-{ctag:waboot_woocommerce_is_shop}")
+	);
+
+	$body_layouts = \WBF\modules\options\of_add_default_key(_get_available_body_layouts());
+	$behaviors[] = array(
+		"name" => "layout",
+		"title" => __("Body layout","waboot"),
+		"desc" => __("Default body layout for posts and pages","waboot"),
+		"options" => $body_layouts['values'],
+		"type" => "select",
+		"default" => $body_layouts['default'],
+		"valid" => array("page","post","-{blog}","{cpt}","-slideshow","-{ctag:waboot_woocommerce_is_shop}"),
+	);
+
+	$behaviors[] = array(
+		'name' => 'primary-sidebar-size',
+		'title' => __("Primary Sidebar width","waboot"),
+		'desc' => __("Choose the primary sidebar width","waboot"),
+		'type' => "select",
+		'options' => array(
+			array(
+				"name" => __("1/2","waboot"),
+				"value" => "1/2"
+			),
+			array(
+				"name" => __("1/3","waboot"),
+				"value" => "1/3"
+			),
+			array(
+				"name" => __("1/4","waboot"),
+				"value" => "1/4"
+			),
+			array(
+				"name" => __("1/6","waboot"),
+				"value" => "1/6"
+			)
+		),
+		"default" => "1/4",
+		"valid" => array('*','-slideshow',"-{ctag:waboot_woocommerce_is_shop}","-{blog}")
+	);
+
+	$behaviors[] = array(
+		'name' => 'secondary-sidebar-size',
+		'title' => __("Secondary Sidebar width","waboot"),
+		'desc' => __("Choose the secondary sidebar width","waboot"),
+		'type' => "select",
+		'options' => array(
+			array(
+				"name" => __("1/2","waboot"),
+				"value" => "1/2"
+			),
+			array(
+				"name" => __("1/3","waboot"),
+				"value" => "1/3"
+			),
+			array(
+				"name" => __("1/4","waboot"),
+				"value" => "1/4"
+			),
+			array(
+				"name" => __("1/6","waboot"),
+				"value" => "1/6"
+			)
+		),
+		"default" => "1/4",
+		"valid" => array('*','-slideshow',"-{ctag:waboot_woocommerce_is_shop}","-{blog}")
+	);
+
+	/***********************************************
+	 ***************** SAMPLES *********************
+	 ***********************************************/
+
+	/**
+	 * SINGLE CHECKBOX
+	 */
+	/*$behaviors[] = array(
+		"name" => "testcheck",
+		"title" => "Test Checkboxes",
+		"desc" => "This is a test checkbox",
+		"type" => "checkbox",
+		"default" => "1",
+		"valid" => array("post","page")
+	);*/
+
+	/**
+	 * MULTIPLE CHECKBOX
+	 */
+	/*$behaviors[] = array(
+		"name" => "testmulticheck",
+		"title" => "Test Checkboxes",
+		"desc" => "This is a test checkbox",
+		"type" => "checkbox",
+		"options" => array(
+			array(
+				"name" => "test1",
+				"value" => "test1"
+			),
+			array(
+				"name" => "test2",
+				"value" => "test2"
+			),
+		),
+		"default" => "test1",
+		"valid" => array("post","page")
+	);*/
+
+	/**
+	 * RADIO
+	 */
+	/*$behaviors[] = array(
+		"name" => "testradio",
+		"title" => "Test Radio",
+		"desc" => "This is a test radio",
+		"type" => "radio",
+		"options" => array(
+			array(
+				"name" => "test1",
+				"value" => "test1"
+			),
+			array(
+				"name" => "test2",
+				"value" => "test2"
+			),
+		),
+		"default" => "test2",
+		"valid" => array("post","page")
+	);*/
+
+	/**
+	 * TEXT
+	 */
+	/*$behaviors[] = array(
+		"name" => "testinput",
+        "title" => "Test Input",
+        "desc" => "This is a test input",
+        "type" => "text",
+        "default" => "testme!",
+        "valid" => array("post","page")
+	);*/
+
+	/**
+	 * TEXTAREA
+	 */
+	/*$behaviors[] = array(
+		"name" => "testarea",
+        "title" => "Test Input",
+        "desc" => "This is a test textarea",
+        "type" => "textarea",
+        "default" => "testme!",
+        "valid" => array("post","page")
+	);*/
+
+	return $behaviors;
+}
+
+/**
+ * Get options for available body layouts in behaviors and options
+ *
+ * @return mixed
+ */
+function _get_available_body_layouts(){
+
+	$imagepath = get_template_directory_uri() . '/assets/images/options/';
+
+	return apply_filters("waboot/layout/options/available_body_layouts",[
+		[
+			"name" => __("No sidebar","waboot"),
+			"value" => "full-width",
+			"thumb"   => $imagepath . "behaviour/no-sidebar.png"
+		],
+		[
+			"name" => __("Sidebar right","waboot"),
+			"value" => "sidebar-right",
+			"thumb"   => $imagepath . "behaviour/sidebar-right.png"
+		],
+		[
+			"name" => __("Sidebar left","waboot"),
+			"value" => "sidebar-left",
+			"thumb"   => $imagepath . "behaviour/sidebar-left.png"
+		],
+		[
+			"name" => __("2 Sidebars","waboot"),
+			"value" => "two-sidebars",
+			"thumb"   => $imagepath . "behaviour/sidebar-left-right.png"
+		],
+		[
+			"name" => __("2 Sidebars right","waboot"),
+			"value" => "two-sidebars-right",
+			"thumb"   => $imagepath . "behaviour/sidebar-right-2.png"
+		],
+		[
+			"name" => __("2 Sidebars left","waboot"),
+			"value" => "two-sidebars-left",
+			"thumb"   => $imagepath . "behaviour/sidebar-left-2.png"
+		],
+		'_default' => 'sidebar-right'
+	]);
 }
