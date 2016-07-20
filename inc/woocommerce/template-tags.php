ec3b2bfe14b0aa25a91d5f28e07eab8ef2458c9d
lostcore@gmail.com <123qw45>
2016-07-20 14:59:16 +0200

    Woocommerce

--- inc/woocommerce/template-tags.php
+++ inc/woocommerce/template-tags.php
@@ -12,3 +12,47 @@ namespace Waboot\woocommerce;
 function is_shop($id){
 	return $id == wc_get_page_id( 'shop' );
 }
+
+/**
+ * Prints out the shop title
+ *
+ * @param string $prefix
+ * @param string $suffix
+ * @param bool $display
+ *
+ * @return string
+ */
+function shop_title($prefix = "", $suffix = "", $display = true) {
+	if (of_get_option('waboot_blogpage_displaytitle') == "1") {
+		$title = $prefix . apply_filters('waboot_index_title_text', get_wc_shop_page_title()) . $suffix;
+	} else {
+		$title = "";
+	}
+
+	if ($display) {
+		echo $title;
+	}
+	return $title;
+}
+
+/**
+ * Format WC archives page title
+ *
+ * @param string $prefix
+ * @param string $suffix
+ * @param bool|true $display
+ *
+ * @return string|void
+ */
+function archive_page_title($prefix = "", $suffix = "", $display = true){
+	if (of_get_option('waboot_woocommerce_displaytitle') == "1") {
+		$output = \Waboot\functions\get_archive_page_title();
+		$output = $prefix.$output.$suffix;
+	}else{
+		$output = "";
+	}
+	if($display){
+		echo $output;
+	}
+	return $output;
+}
