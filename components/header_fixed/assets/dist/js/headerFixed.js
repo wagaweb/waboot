0a8e8ac1b799162ebdb55167e95f582458128a38
IacopoLea <iacopo@waga.it>
2016-07-22 17:19:59 +0200

    component header fixed - atest su funzione breakpoint

--- components/header_fixed/assets/dist/js/headerFixed.js
+++ components/header_fixed/assets/dist/js/headerFixed.js
@@ -2,9 +2,8 @@ jQuery(document).ready(function($){
 
 
     var fixedHeader = $(wbHeaderFixed.fixed_class),
-        fixedOnStart = wbHeaderFixed.fixed_on_start,
-        enterScroll = wbHeaderFixed.scroll_enter,
-        afterScroll = wbHeaderFixed.enter_after;
+        mode = wbHeaderFixed.modality,
+        breakpoint = wbHeaderFixed.breakpoint;
 
     var styleBefore = {
         'padding-top' : wbHeaderFixed.padding_before+'px',
@@ -23,60 +22,67 @@ jQuery(document).ready(function($){
      * first case
      * we have a fixed header form the beginning
      */
-    if (fixedOnStart) {
-        // add the class fixed_header_component
-        fixedHeader.addClass('fixed_header_component');
-        // and if the user is on top of the page apply style before,
-        enterFixed();
-        // now we can change the css checking if the position of the window is before or after the breakpoint specified by the user
-        $( window ).scroll(function() {
-            enterFixed();
-        });
-    /**
-     * Second case,
-     * we have header entering on scroll up
-     */
-    } else if (enterScroll) {
-        // da dove partiamo
-        var initialScroll = $(document).scrollTop();
-        $( window ).scroll(function() {
-            enterScrollUp();
-        });
-    /**
-     * Last case,
-     * the header enter after the breakpoint specified by the user
-     */
-    } else {
-        // at the document ready apply the correct class
-        enterafter();
+    switch(mode) {
 
-        // then update the classes on window scroll
-        $( window ).scroll(function() {
-            enterafter();
-        });
+        case 'beginning':
+
+            fixedHeader.addClass('fixed-header-component');
+            enterFixed();
+            // now we can change the css checking if the position of the window is before or after the breakpoint specified by the user
+            $( window ).scroll(function() {
+                enterFixed();
+            });
+
+            break;
+
+
+
+        case 'scrollUp':
+
+            // da dove partiamo
+            var initialScroll = $(document).scrollTop();
+            $( window ).scroll(function() {
+                enterScrollUp();
+            });
+
+            break;
+
+
+
+        case 'breakpoint':
+
+            // First apply the correct padding and background
+            fixedHeader.css(styleBefore).addClass('margin-top-animation');
+            // at the document ready apply the correct class
+            enterAfter();
+
+            // then update the classes on window scroll
+            $( window ).scroll(function() {
+                enterAfter();
+            });
+
     }
-    /**
-     *
-     *
-     *
-     *
-     */
+
+    
+    
+
+
     function enterScrollUp() {
         var currentScroll = $(this).scrollTop(), // lo scroll attuale
-            headerHeight = fixedHeader.height(),
+            headerHeight = fixedHeader.outerHeight(),
             sensitiveness = 15, // voglio filtrare per i movimenti decisi
             delta = currentScroll - initialScroll; // la differenza fra lo scroll attuale e il punto di partenza
         if (Math.abs(delta) > sensitiveness) {
             if (delta > 0) { // se il delta è maggiore di 0 stiamo andando giù
-                // fixedHeader.removeClass('fixed_header_component');
+                // fixedHeader.removeClass('fixed-header-component');
                 fixedHeader.css('margin-top', headerHeight * -1);
             } else if (delta < 0) { // altrimenti stiamo andando su
-                fixedHeader.addClass('fixed_header_component').css('margin-top', 0);
+                fixedHeader.addClass('fixed-header-component').css('margin-top', 0);
             }
         }
         // anyhow just reset margin and class if we are at the top
         if (currentScroll<=headerHeight) {
-            fixedHeader.css('margin-top', 0);
+            fixedHeader.css('margin-top', $(this).scrollTop()).removeClass('fixed-header-component');
         }
         initialScroll = currentScroll; // a ogni scroll dobbiamo aggiornare la posizione iniziale attualizzandola con la posizione corrente
 
@@ -84,35 +90,37 @@ jQuery(document).ready(function($){
 
     function enterFixed() {
         var scroll = $(document).scrollTop();
-        if (scroll > afterScroll) {
+        if (scroll > breakpoint) {
             fixedHeader.css(styleAfter);
         } else {
             fixedHeader.css(styleBefore);
         }
     }
 
-    function enterafter() {
-        var scroll = $(document).scrollTop();
-        if (scroll < afterScroll && scroll <= fixedHeader.height()) {
-            fixedHeader.removeClass('fixed_header_component');
-            fixedHeader.css('margin-top', 0);
-        } else if (scroll < afterScroll && scroll > fixedHeader.height()) {
-            fixedHeader.removeClass('fixed_header_component');
-            fixedHeader.css('margin-top', fixedHeader.height()*-1);
+    function enterAfter() {
+        var scroll = $(document).scrollTop(),
+            headerHeight = fixedHeader.outerHeight();
+            console.log(scroll);
+        if (scroll < breakpoint && scroll <= headerHeight) {
+            fixedHeader.removeClass('fixed-header-component');
+            fixedHeader.css('margin-top', 0).css(styleBefore);
+            $('.main-wrapper').css('padding-top', 0);
+
+
+
+        } else if (scroll < breakpoint && scroll > headerHeight) {
+            // fixedHeader.removeClass('fixed-header-component');
+
+            fixedHeader.css('margin-top', headerHeight*-1);
+            $('.main-wrapper').css('padding-top', headerHeight);
+
+            setTimeout(function(){
+                fixedHeader.addClass('fixed-header-component');
+            }, 400);
+
+
         } else {
-            fixedHeader.addClass('fixed_header_component').css('margin-top', 0);
+            fixedHeader.addClass('fixed-header-component').css('margin-top', 0);
         }
     }
 });
-
-
-/*
- wbHeaderFixed.fixed_class
- wbHeaderFixed.fixed_on_start
- wbHeaderFixed.color_before
- wbHeaderFixed.padding_before
- wbHeaderFixed.color_after
- wbHeaderFixed.padding_after
- wbHeaderFixed.scroll_enter
- wbHeaderFixed.enter_after
- */
