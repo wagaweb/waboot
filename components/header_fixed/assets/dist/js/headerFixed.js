922a04b76f140391f9ae82414d0f0e047d67eb0f
fabosway <fabio@waga.it>
2016-08-30 17:55:29 +0200

    Fix Component

--- components/header_fixed/assets/dist/js/headerFixed.js
+++ components/header_fixed/assets/dist/js/headerFixed.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function($){
 
-
     var fixedHeader = $(wbHeaderFixed.fixed_class),
         mode = wbHeaderFixed.modality,
         breakpoint = wbHeaderFixed.breakpoint,
@@ -34,57 +33,38 @@ jQuery(document).ready(function($){
             $( window ).scroll(function() {
                 enterFixed();
             });
-
-            break;
-
-
-        case 'scrollUp':
-
-            // da dove partiamo
-            var initialScroll = $(document).scrollTop(),
-                sensitiveness = 15; // voglio filtrare per i movimenti decisi
-            fixedHeader.addClass('transition-all');
-            $( window ).scroll(function() {
-                enterScrollUp(sensitiveness);
-            });
-
             break;
 
 
         case 'breakpoint':
-
             fixedHeader.addClass('top-animation');
             enterAfter();
             // then update the classes on window scroll
             $( window ).scroll(function() {
                 enterAfter();
             });
-
             break;
+
+
+        case 'scrollUp':
+            // da dove partiamo
+            var initialScroll = $(document).scrollTop();
+            fixedHeader.addClass('transition-all');
+            $( window ).scroll(function() {
+                enterScrollUp();
+            });
+            break;
+
     }
 
+    // Main Wrapper Padding Top
 
-    function enterScrollUp(sensitiveness) {
-        var currentScroll = $(this).scrollTop(), // lo scroll attuale
-            delta = currentScroll - initialScroll, // la differenza fra lo scroll attuale e il punto di partenza
-            newHeaderHeight = fixedHeader.outerHeight();
-
-        if (delta > 0) { // se il delta è maggiore di 0 stiamo andando giù
-            // fixedHeader.removeClass('fixed-header-component');
-            fixedHeader.css('margin-top', newHeaderHeight * -1);
-        } else if (delta < 0 && Math.abs(delta) > sensitiveness) { // altrimenti stiamo andando su
-            fixedHeader.css('margin-top', 0).css(styleAfter);
-        }
-
-        // anyhow just reset margin and class if we are at the top
-        if (currentScroll < breakpoint && currentScroll <= newHeaderHeight) {
-            fixedHeader.css({
-                'top': scroll * -1,
-                'margin-top': 0
-            }).css(styleBefore);
-        }
-        initialScroll = currentScroll; // a ogni scroll dobbiamo aggiornare la posizione iniziale attualizzandola con la posizione corrente
+    if (window.matchMedia('(max-width: 991px)').matches) {
+        $('.main-wrapper').css('padding-top', fixedHeader.height()+20+'px');
+    }else{
+        $('.main-wrapper').css('padding-top', fixedHeader.height()+(wbHeaderFixed.padding_before*2)+20+'px');
     }
+
 
 
     function enterFixed() {
@@ -103,20 +83,58 @@ jQuery(document).ready(function($){
 
         if (scroll < breakpoint && scroll <= newHeaderHeight) {
             fixedHeader.stop().css({
-                    'top': scroll*-1,
-                    'margin-top': 0
+                'top': scroll*-1
             }).css(styleBefore);
         } else if (scroll < breakpoint && scroll > newHeaderHeight) {
             fixedHeader.stop().css({
-                'top': newHeaderHeight*-1,
-                'margin-top': 0
+                'top': newHeaderHeight*-1
             });
         } else {
             fixedHeader.stop().css({
-                'margin-top': 0,
                 'top': 0
             }).css(styleAfter);
         }
     }
-    //
+
+
+    function enterScrollUp() {
+
+        var currentScroll = $(this).scrollTop(), // lo scroll attuale
+            delta = currentScroll - initialScroll, // la differenza fra lo scroll attuale e il punto di partenza
+            newHeaderHeight = fixedHeader.outerHeight();
+
+        if (delta < 0 && currentScroll > newHeaderHeight) {
+            fixedHeader.stop().css({
+                'top': 0
+            }).css(styleAfter).addClass('navstyleafter');
+        } else {
+            fixedHeader.stop().css({
+                'top': currentScroll*-1
+            }).css(styleBefore).removeClass('navstyleafter');
+        }
+
+        /*
+        var currentScroll = $(this).scrollTop(), // lo scroll attuale
+            delta = currentScroll - initialScroll, // la differenza fra lo scroll attuale e il punto di partenza
+            newHeaderHeight = fixedHeader.outerHeight();
+
+        if (delta > 0) { // se il delta è maggiore di 0 stiamo andando giù
+            // fixedHeader.removeClass('fixed-header-component');
+            fixedHeader.css('margin-top', newHeaderHeight * -1);
+        } else if (delta < 0 && Math.abs(delta) > sensitiveness) { // altrimenti stiamo andando su
+            fixedHeader.css('margin-top', 0).css(styleAfter);
+        }
+
+        // anyhow just reset margin and class if we are at the top
+        if (currentScroll < breakpoint && currentScroll <= newHeaderHeight) {
+            fixedHeader.css({
+                'top': scroll * -1,
+                'margin-top': 0
+            }).css(styleBefore);
+        }*/
+
+        initialScroll = currentScroll; // a ogni scroll dobbiamo aggiornare la posizione iniziale attualizzandola con la posizione corrente
+    }
+
+
 });
