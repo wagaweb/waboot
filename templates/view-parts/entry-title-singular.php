34a1d303bead111bf5bf40b9f1755a1810d93640
fabosway <fabio@waga.it>
2016-08-31 19:36:09 +0200

    Add Title Div wrapper

--- templates/view-parts/entry-title-singular.php
+++ templates/view-parts/entry-title-singular.php
@@ -3,4 +3,12 @@
  * Waboot View 
  */
 ?>
-<?php \Waboot\template_tags\wrapped_title('<h1 class="page-title">','</h1>',$title);
+<div class="title-wrapper">
+    <?php if(\Waboot\functions\get_behavior('title-position','bottom') == 'top') : ?>
+        <div class="container">
+    <?php endif; ?>
+        <?php \Waboot\template_tags\wrapped_title('<h1 class="page-title">','</h1>',$title); ?>
+    <?php if(\Waboot\functions\get_behavior('title-position','bottom') == 'top') : ?>
+        </div>
+    <?php endif; ?>
+</div>
