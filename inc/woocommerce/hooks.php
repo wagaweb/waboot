e284ca1689e3d708dadada1f266e9912f21d6547
LostCore <lostcore@gmail.com>
2016-09-01 16:59:34 +0200

    Fix namespace

--- inc/woocommerce/hooks.php
+++ inc/woocommerce/hooks.php
@@ -5,9 +5,9 @@ namespace Waboot\woocommerce;
 use WBF\modules\options\Organizer;
 
 function alter_entry_title($title, $current_title_position){
-	if(is_product_category()){
+	if(\is_product_category()){
 		$title = \Waboot\functions\get_archive_page_title();
-	}elseif(is_shop()){
+	}elseif(\is_shop()){
 		$title = get_shop_page_title();
 	}
 	return $title;
@@ -15,11 +15,11 @@ function alter_entry_title($title, $current_title_position){
 add_filter("waboot/entry/title", __NAMESPACE__."\\alter_entry_title", 10, 2);
 
 function alter_entry_title_visibility($is_visible, $current_title_position){
-	if(is_product_category()){
+	if(\is_product_category()){
 		if($current_title_position == "top"){
 			$is_visible = \Waboot\functions\get_option("woocommerce_title_position") == "top" && (bool) \Waboot\functions\get_option("woocommerce_display_title");
 		}
-	}elseif(is_shop()){
+	}elseif(\is_shop()){
 		if($current_title_position == "top"){
 			$is_visible = \Waboot\functions\get_option("woocommerce_title_position") == "top" && (bool) \Waboot\functions\get_option("blog_display_title");
 		}
