57b2fed9e565853cf2497ff243c589f3d58f285e
LostCore <lostcore@gmail.com>
2016-09-09 13:42:48 +0200

    Ripristinate alcune opzioni

--- inc/hooks/entry/entry-footer.php
+++ inc/hooks/entry/entry-footer.php
@@ -1,19 +1,3 @@
 <?php
 
 namespace Waboot\hooks\entry;
-
-/**
- * Prints out the entry footer wrapper start
- */
-function entry_footer_wrapper_start(){
-	echo '<footer class="entry-footer">';
-}
-add_action("waboot/entry/footer",__NAMESPACE__."\\entry_footer_wrapper_start",10);
-
-/**
- * Prints out the entry footer wrapper end
- */
-function entry_footer_wrapper_end(){
-	echo '</footer>';
-}
-add_action("waboot/entry/footer",__NAMESPACE__."\\entry_footer_wrapper_end",9999);
