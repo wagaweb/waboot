9d6b14e95edede9e3f8ad3aec1666e626be454a4
LostCore <lostcore@gmail.com>
2016-09-13 17:07:53 +0200

    Autoload dei template parts per il single.php

--- templates/wordpress/single.php
+++ templates/wordpress/single.php
@@ -1,6 +1,10 @@
 <?php \Waboot\template_tags\post_navigation('nav-above'); ?>
-<?php while ( have_posts() ) : the_post(); ?>
-	<?php get_template_part('templates/wordpress/parts/content', 'single'); ?>
+<?php while ( have_posts() ) : the_post(); $pt = get_post_type(); ?>
+	<?php if(locate_template("templates/wordpress/parts/content-".$pt, false, false) != '') : ?>
+		<?php get_template_part('templates/wordpress/parts/content', $pt); ?>
+	<?php else: ?>
+		<?php get_template_part('templates/wordpress/parts/content', 'single'); ?>
+	<?php endif; ?>
 	<?php
 	if(comments_open() || '0' != get_comments_number()){
 		comments_template('/templates/wordpress/comments.php',true);
