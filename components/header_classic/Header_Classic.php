2a7887a77a1540c4f5a04690e5ff3f356708281f
fabosway <fabio@waga.it>
2016-10-07 15:56:18 +0200

    Try new offCanvas

--- components/header_classic/Header_Classic.php
+++ components/header_classic/Header_Classic.php
@@ -21,6 +21,7 @@ class Header_Classic extends \Waboot\Component{
         parent::styles();
         wp_enqueue_style('header_classic_style', $this->directory_uri . '/assets/dist/css/headerClassic.css');
         wp_enqueue_style('offcanvas_style', $this->directory_uri . '/assets/dist/css/offcanvas.css');
+        // wp_enqueue_script('header_classic_js', $this->directory_uri . '/assets/dist/js/headerClassic.js', ['jquery'], false, true);
         wp_enqueue_script('offcanvas_js', $this->directory_uri . '/assets/dist/js/offcanvas.js', ['jquery'], false, true);
     }
 
