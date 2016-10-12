bc57866c04ed706f4777689ceab7b7cbd1af581a
IacopoLea <iacopo@waga.it>
2016-10-12 16:49:45 +0200

    fix archive template

--- templates/wordpress/archive.php
+++ templates/wordpress/archive.php
@@ -1,8 +1,17 @@
 <?php $vars = \Waboot\functions\get_archives_template_vars(); ?>
-<?php $tx = get_queried_object()->taxonomy; ?>
 
-<?php if(locate_template("templates/wordpress/archive/taxonomy-".$tx.".php", false, false) != '') : ?>
-    <?php get_template_part('templates/wordpress/archive/taxonomy', $tx); ?>
+<?php
+$o = get_queried_object();
+$tpl = "";
+if($o instanceof WP_Term){
+    $tpl = "taxonomy-".$o->taxonomy;
+}elseif($o instanceof WP_Post_Type){
+    $tpl = "archive-".$o->name;
+}
+?>
+
+<?php if(!empty($tpl) && locate_template("templates/wordpress/archive/".$tpl.".php", false, false) != '') : ?>
+    <?php get_template_part('templates/wordpress/archive/'.$tpl); ?>
 <?php else: ?>
     <?php if(have_posts()) : ?>
         <?php if($vars['display_nav_above']) \Waboot\template_tags\post_navigation('nav-above'); ?>
