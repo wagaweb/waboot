2e225532837e5849cb6f319ba261d7205e358575
LostCore <lostcore@gmail.com>
2016-10-19 10:59:11 +0200

    Aggiunti wrapper start\end anche alla template dell'immagine

--- templates/wrapper-start.php
+++ templates/wrapper-start.php
@@ -1,6 +1,6 @@
 <?php
 	$main_wrap_classes = apply_filters( 'waboot_mainwrap_container_class', 'content-area col-sm-8' );
-	if(is_404()){
+	if(is_404() || is_attachment()){
 		$main_wrap_classes = "content-area col-sm-12";
 	}
 ?>
