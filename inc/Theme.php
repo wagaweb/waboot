60902c5399f9874a3a3e5ba049e84e3af8a16ffb
lostcore@gmail.com <123qw45>
2016-11-02 12:04:19 +0100

    Aggiunte le funzioni per gli stili inline

--- inc/Theme.php
+++ inc/Theme.php
@@ -2,6 +2,8 @@
 
 namespace Waboot;
 
+use WBF\components\utils\Utilities;
+
 class Theme{
 	/**
 	 * @var Theme
@@ -19,6 +21,11 @@ class Theme{
 	var $inline_styles;
 
 	/**
+	 * @var \WP_Styles
+	 */
+	var $custom_styles_handler;
+
+	/**
 	 * Returns the *Singleton* instance.
 	 *
 	 * @return Theme The *Singleton* instance.
@@ -32,6 +39,7 @@ class Theme{
 
 	protected function __construct(){
 		$this->layout = Layout::getInstance();
+		$this->custom_styles_handler = new \WP_Styles();
 		add_action("waboot/head/end", [$this,"print_inline_styles"]);
 	}
 
@@ -58,15 +66,48 @@ class Theme{
 		return $this;
 	}
 
+	/**
+	 * Adds a new inline style. Inline styles will be printed during "waboot/head/end" action.
+	 *
+	 * @param $handle
+	 * @param $path
+	 */
 	public function add_inline_style($handle,$path){
-		$this->inline_styles[$handle] = $path;
+		if(preg_match("/^https?/",$path)){
+			$path = Utilities::url_to_path($path);
+		}
+		if(!file_exists($path)) return;
+		$this->inline_styles[] = $handle;
+		$this->custom_styles_handler->add($handle,$path);
 	}
 
+	/**
+	 * Print registered inline styles
+	 *
+	 * @hooked "waboot/head/end"
+	 */
 	public function print_inline_styles(){
 		$output = "";
-		foreach ($this->inline_styles as $style){
-
+		/*
+		 * We enqueue the registered inline styles. We hope that those steps will resolve dependencies
+		 */
+		$this->custom_styles_handler->all_deps($this->inline_styles);
+		$items = $this->custom_styles_handler->to_do;
+		/*
+		 * We cycle through the registered styles. We suppose that those styles are already ordered by dependency (it's the reason we used WP_Styles in the first place)
+		 */
+		foreach ($items as $style){
+			if(isset($this->custom_styles_handler->registered[$style])){
+				$current_style = $this->custom_styles_handler->registered[$style];
+				//Read the file:
+				$content = file_get_contents($current_style->src);
+				//Append:
+				if($content){
+					$output .= $content."\n";
+				}
+			}
 		}
+		$output = sprintf( "<style id='%s-inline-css' type='text/css'>\n%s\n</style>\n", "components", $output );
 		echo $output;
 	}
 
