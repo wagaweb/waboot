8c67208874055a3caa1038632d741b40fe4d854e
IacopoLea <iacopo@waga.it>
2016-11-09 10:30:24 +0100

    fix mega bug in index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 		Waboot()->layout->render_zone("content");
 	}catch(Exception $error){
 		$e = new \WBF\components\mvc\HTMLView("templates/view-parts/content-errors.php");
-		$e->clean()->display(['Error' => $e,'message' => $error->getMessage()]);
+		$e->clean()->display(['Error' => $error,'message' => $error->getMessage()]);
 	}
 	?>
 	<?php get_template_part("templates/wrapper","end"); ?>
