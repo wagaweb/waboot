ecff9747a5d3498af6ea7aaa258ad7d319c02859
LostCore <lostcore@gmail.com>
2016-12-20 10:34:02 +0100

    Wrapper moved outside the while

--- templates/wordpress/blog.php
+++ templates/wordpress/blog.php
@@ -8,11 +8,11 @@
 <?php endif; ?>
 <?php if(have_posts()): ?>
 	<?php \Waboot\template_tags\post_navigation( 'nav-above' ); // display content nav above posts if needed ?>
+    <div class="<?php \Waboot\template_tags\posts_wrapper_class(); ?>">
 	<?php while(have_posts()) :  the_post(); ?>
-		<div class="<?php \Waboot\template_tags\posts_wrapper_class(); ?>">
 		<?php \Waboot\functions\get_template_part( '/templates/wordpress/parts/content', get_post_format() ); ?>
-		</div>
 	<?php endwhile; ?>
+    </div>
 	<?php \Waboot\template_tags\post_navigation( 'nav-below' ); // display content nav below posts if needed ?>
 <?php else: ?>
 	<?php get_template_part('/templates/parts/content', 'none'); // No results ?>
