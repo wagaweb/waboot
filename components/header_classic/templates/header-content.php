200faa6bf959897ee757e725e1dda6da83461e6f
fabosway <fabio@waga.it>
2017-01-04 16:06:06 +0100

    Fix Link in Header Classic Logo

--- components/header_classic/templates/header-content.php
+++ components/header_classic/templates/header-content.php
@@ -2,7 +2,7 @@
     <div class="header-logo">
         <a href="<?php echo home_url( '/' ); ?>">
             <?php if ( \Waboot\template_tags\get_desktop_logo() != "" ) : ?>
-                <?php \Waboot\template_tags\desktop_logo(true); ?>
+                <?php \Waboot\template_tags\desktop_logo(); ?>
             <?php else : ?>
                 <?php \Waboot\template_tags\site_title(); ?>
             <?php endif; ?>
