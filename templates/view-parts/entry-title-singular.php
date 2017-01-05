647010dab6774026f8f9473be673d241be9c14f7
clinic <weblab@gmail.com>
2017-01-05 16:20:40 +0100

    Added entry-title class

--- templates/view-parts/entry-title-singular.php
+++ templates/view-parts/entry-title-singular.php
@@ -7,7 +7,7 @@
     <?php if(\Waboot\functions\get_behavior('title-position') == 'top') : ?>
         <div class="container">
     <?php endif; ?>
-    <?php \Waboot\template_tags\wrapped_title('<h1 class="page-title">','</h1>',$title); ?>
+    <?php \Waboot\template_tags\wrapped_title('<h1 class="page-title entry-title">','</h1>',$title); ?>
     <?php if(\Waboot\functions\get_behavior('title-position') == 'top') : ?>
         </div>
     <?php endif; ?>
