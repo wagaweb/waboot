a9e7ba2844631becf1058be56a0aefcfd60bdc62
fabosway <fabio@waga.it>
2017-01-25 19:06:01 +0100

    Add MoseFx in Header Splitted Menu Component

--- components/header_splitted_menu/Header_Splitted_Menu.php
+++ components/header_splitted_menu/Header_Splitted_Menu.php
@@ -7,7 +7,7 @@ Tags: Header
 Version: 1.0
 Author: WAGA Team <dev@waga.it>
 Author URI: http://www.waga.it
-*/
+ */
 
 if(!class_exists("\\Waboot\\Component")) return;
 
@@ -37,29 +37,32 @@ class Header_Splitted_Menu extends \Waboot\Component{
     public function display_tpl(){
 
         // retrieve the user options
-	    $menu_position = (\Waboot\functions\get_option('header_splitted_menu'))
-		    ? \Waboot\functions\get_option('header_splitted_menu')
-		    : 'main';
-	    $split_position = (\Waboot\functions\get_option('header_splitted_position'))
-		    ? \Waboot\functions\get_option('header_splitted_position')
-		    : '';
+        $menu_position = (\Waboot\functions\get_option('header_splitted_menu'))
+            ? \Waboot\functions\get_option('header_splitted_menu')
+            : 'main';
+        $split_position = (\Waboot\functions\get_option('header_splitted_position'))
+            ? \Waboot\functions\get_option('header_splitted_position')
+            : '';
 
-	    $walker = new WabootSplittedNavMenuWalker( $split_position, $menu_position);
+        $walker = new WabootSplittedNavMenuWalker( $split_position, $menu_position);
 
-    	$menu = new \WBF\components\mvc\HTMLView($this->theme_relative_path."/templates/header_splitted.php");
+        $menu = new \WBF\components\mvc\HTMLView($this->theme_relative_path."/templates/header_splitted.php");
         $menu->clean()->display([
-	        'walker'    => $walker
+            'walker'    => $walker
         ]);
     }
 
 
-	/**
-	 * Register component scripts (called automatically)
-	 */
+    /**
+     * Register component scripts (called automatically)
+     */
     public function scripts(){
-	    $additional_margin = (\Waboot\functions\get_option('header_splitted_margin'))
-		    ? \Waboot\functions\get_option('header_splitted_margin')
-		    : 10;
+        $additional_margin = (\Waboot\functions\get_option('header_splitted_margin'))
+            ? \Waboot\functions\get_option('header_splitted_margin')
+            : 10;
+
+        $is_homepage = \WBF\components\utils\Utilities::is_default_home() || \WBF\components\utils\Utilities::is_static_home();
+        $mosefx_enabled = \Waboot\functions\get_option('header_splitted_position_mosefx_enabled', false);
 
         $scripts = [
             'component-header_splitted_menu' => [
@@ -70,7 +73,15 @@ class Header_Splitted_Menu extends \Waboot\Component{
                 'i10n' => [
                     'name' => "wabootHeaderSplitted",
                     'params' => [
-                        'margin' => $additional_margin
+                        'margin' => $additional_margin,
+                        'split_enabled' => call_user_func(function() use($is_homepage, $mosefx_enabled){
+                            if($is_homepage && $mosefx_enabled){
+                                return false;
+                            }
+                            return true;
+                        }),
+                        'is_homepage' => $is_homepage,
+                        'mosefx_enabled' => $mosefx_enabled
                     ]
                 ]
             ]
@@ -81,63 +92,73 @@ class Header_Splitted_Menu extends \Waboot\Component{
     }
 
 
-	/**
-	 * Register component styles (called automatically)
-	 */
+    /**
+     * Register component styles (called automatically)
+     */
     public function styles(){
         //wp_enqueue_style('component-header_splitted-style', $this->directory_uri . '/assets/dist/css/headerSplittedMenu.css');
-	    Waboot()->add_inline_style('component-header_splitted-style', $this->directory_uri . '/assets/dist/css/headerSplittedMenu.css');
+        Waboot()->add_inline_style('component-header_splitted-style', $this->directory_uri . '/assets/dist/css/headerSplittedMenu.css');
     }
 
 
-	/**
-	 * Register component widgets (called automatically).
-	 *
-	 * @hooked 'widgets_init'
-	 */
-	public function widgets(){
-		//register_widget("sampleWidget");
-	}
+    /**
+     * Register component widgets (called automatically).
+     *
+     * @hooked 'widgets_init'
+     */
+    public function widgets(){
+        //register_widget("sampleWidget");
+    }
 
-	/**
-	 * This is an action callback.
-	 *
-	 * Here you can use WBF Organizer to set component options
-	 */
+    /**
+     * This is an action callback.
+     *
+     * Here you can use WBF Organizer to set component options
+     */
     public function register_options() {
-	    parent::register_options();
+        parent::register_options();
 
-	    $orgzr = \WBF\modules\options\Organizer::getInstance();
-	    $orgzr->set_group($this->name."_component");
-	    $orgzr->add_section("header",_x("Header","Theme options section","waboot"));
+        $orgzr = \WBF\modules\options\Organizer::getInstance();
+        $orgzr->set_group($this->name."_component");
+        $orgzr->add_section("header",_x("Header","Theme options section","waboot"));
 
-	    $orgzr->add([
-		    'name' => 'Header Splitted',
-		    'desc' => __( 'Edit default options for Header Fixed post type', 'waboot' ),
-		    'type' => 'info'
-	    ], "header");
-	    $orgzr->update('header_splitted_position',[
-			    'name' => __( 'Menu Position', 'waboot' ),
-                'desc' => __( 'Select the item of the menu at which you want to apply the margin. By default split at the middle.', 'waboot' ),
-			    'id'   => $this->name.'_item_select',
-			    'type' => 'text',
-                'std' => ''
-		    ],"header");
-	    $orgzr->update('header_splitted_margin',[
-			    'name' => __( 'Additional Margin', 'waboot' ),
-			    'desc' => __( 'An additional margin to increase spacing between logo and menu items. This number is applied to both sides of the logo, therefore consider it will be doubled. Default is 10px', 'waboot' ),
-			    'id'   => $this->name.'_margin_select',
-			    'type' => 'text'
-		    ],"header");
-	    $orgzr->update('header_splitted_menu',[
-			    'name' => __( 'Menu Theme Position', 'waboot' ),
-			    'desc' => __( 'The Theme position of the menu that will be splitted, Default is "main"', 'waboot' ),
-			    'id'   => $this->name.'_menu_select',
-			    'type' => 'text'
-		    ],"header");
+        $orgzr->add([
+            'name' => 'Header Splitted',
+            'desc' => __( 'Edit default options for Header Fixed post type', 'waboot' ),
+            'type' => 'info'
+        ], "header");
 
-	    $orgzr->reset_group();
-	    $orgzr->reset_section();
+        $orgzr->add([
+            'name' => __( 'Menu Position', 'waboot' ),
+            'desc' => __( 'Select the item of the menu at which you want to apply the margin. By default split at the middle.', 'waboot' ),
+            'id'   => 'header_splitted_position',
+            'type' => 'text',
+            'std' => ''
+        ],"header");
+
+        $orgzr->add([
+            'name' => __( 'Additional Margin', 'waboot' ),
+            'desc' => __( 'An additional margin to increase spacing between logo and menu items. This number is applied to both sides of the logo, therefore consider it will be doubled. Default is 10px', 'waboot' ),
+            'id'   => 'header_splitted_margin',
+            'type' => 'text'
+        ],"header");
+
+        $orgzr->add([
+            'name' => __( 'Menu Theme Position', 'waboot' ),
+            'desc' => __( 'The Theme position of the menu that will be splitted, Default is "main"', 'waboot' ),
+            'id'   => 'header_splitted_menu',
+            'type' => 'text'
+        ],"header");
+
+        $orgzr->add([
+            'name' => __( 'Enable Mosefx in Homepage', 'waboot' ),
+            'desc' => __( 'This option will exclude the standard split effect in homepage. You have to enable mosefx in home by adding some additional code. See component documentation.', 'waboot' ),
+            'id'   => 'header_splitted_position_mosefx_enabled',
+            'type' => 'checkbox'
+        ],"header");
+
+        $orgzr->reset_group();
+        $orgzr->reset_section();
     }
 
     public function onActivate(){
--- components/header_splitted_menu/assets/dist/js/headerSplittedMenu.js
+++ components/header_splitted_menu/assets/dist/js/headerSplittedMenu.js
@@ -1,15 +1,58 @@
-jQuery(document).ready(function($){
+function Waboot_HeaderSplittedMenu(params){
+    var $ = jQuery;
+    var defaults_params = {
+        positionTop: jQuery(window).height() / 2,
+        widthStart: 400,
+        widthEnd: 200,
+        heightEnd: 0,
+        splitStart: 0,
+        splitEnd: 100
+    };
 
-    defineLogo();
+    this.params = $.extend(defaults_params,params);
 
-    $(window).resize(function() {
-        defineLogo()
-    });
+    var self = this;
 
-    /**
-     * Define Logo
-     */
-    function defineLogo() {
+    this.mosefx = function(){
+        var $ = jQuery;
+
+        var logo = $('.logonav');
+        var logoImg = $('.logonav img');
+        var navLeft = $('.main-navigation .navbar-split-left');
+        var navRight = $('.main-navigation .navbar-split-right');
+
+        var positionLeftStart = (self.params.widthStart / 2) * -1;
+        var positionLeftEnd = (self.params.widthEnd / 2) * -1;
+
+        navLeft.css('padding-top', self.params.heightEnd + 'px');
+        navLeft.css('padding-bottom', self.params.heightEnd + 'px');
+        navRight.css('padding-top', self.params.heightEnd + 'px');
+        navRight.css('padding-bottom', self.params.heightEnd + 'px');
+
+        logo.css('top', self.params.positionTop);
+        logoImg.css('width', self.params.widthStart);
+        logo.css('margin-left', positionLeftStart);
+
+        $(window).scroll(function () {
+            if ($(window).scrollTop() < self.params.positionTop) {
+                var scrollTop = $(window).scrollTop();
+                logo.css('top', self.params.positionTop - scrollTop + 'px');
+                logoImg.css('width', self.params.widthStart - scrollTop * ( (self.params.widthStart - self.params.widthEnd) / self.params.positionTop ) + 'px');
+                logo.css('margin-left', positionLeftStart - scrollTop * ( (positionLeftStart - positionLeftEnd) / self.params.positionTop ) + 'px');
+                navLeft.css('padding-right', self.params.splitStart - scrollTop * ( (self.params.splitStart - self.params.splitEnd) / self.params.positionTop ) + 'px');
+                navRight.css('padding-left', self.params.splitStart - scrollTop * ( (self.params.splitStart - self.params.splitEnd) / self.params.positionTop ) + 'px');
+            } else {
+                logo.css('top', '0px');
+                logoImg.css('width', self.params.widthEnd + 'px');
+                logo.css('margin-left', positionLeftEnd + 'px');
+                navLeft.css('padding-right', self.params.splitEnd + 'px');
+                navRight.css('padding-left', self.params.splitEnd + 'px');
+            }
+        });
+    };
+
+    this.split = function(){
+        var $ = jQuery;
         var logocontainer = $('.logonav'),
             logo = $('.logonav img'),
             width = logo.width(),
@@ -20,7 +63,6 @@ jQuery(document).ready(function($){
 
         logocontainer.css('margin-left', (width/2)*-1);
 
-        // add top nav height, if any
         if (height > 50) {
             navLeft.css('padding-top', (height-50)/2);
             navRight.css('padding-top', (height-50)/2);
@@ -35,5 +77,15 @@ jQuery(document).ready(function($){
 
         navLeft.css('padding-right', width/2+additionalMargin);
         navRight.css('padding-left', width/2+additionalMargin);
+    }
+}
+
+jQuery(document).ready(function($){
+    if(wabootHeaderSplitted.split_enabled){
+        var wbhsm = new Waboot_HeaderSplittedMenu();
+        wbhsm.split();
+        $(window).resize(function() {
+            wbhsm.split();
+        });
     }
 });
