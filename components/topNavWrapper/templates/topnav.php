e13297b3044f7bc7de72b585f138c266ded71913
fabosway <fabio@waga.it>
2017-02-09 17:15:59 +0100

    Fix TopNavWrapper Component

--- components/topNavWrapper/templates/topnav.php
+++ components/topNavWrapper/templates/topnav.php
@@ -1,5 +1,5 @@
 <div id="topnav-wrapper" class="topnav-wrapper">
-    <div id="topnav-inner" class="<?php echo Waboot\functions\get_option( 'topnav_width','container-fluid' ); ?> ">
+    <div id="topnav-inner" class="topnav-inner <?php echo Waboot\functions\get_option( 'topnav_width','container-fluid' ); ?> ">
         <?php
         // Top Nav widgets
         \Waboot\functions\print_widgets_in_area('topnav');
