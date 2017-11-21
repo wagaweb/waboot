4a1785b32db32c2802a3d1d8cc0d1dafde0c4c8e
LostCore <lostcore@gmail.com>
2017-11-21 12:22:25 +0100

    Updated to 3.0.0

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -2,11 +2,18 @@
 /**
  * The template for displaying product content in the single-product.php template
  *
- * Override this template by copying it to yourtheme/woocommerce/content-single-product.php
+ * This template can be overridden by copying it to yourtheme/woocommerce/content-single-product.php.
  *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     1.6.4
+ * @version     3.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -19,15 +26,15 @@ global $post, $product;
 
 <?php
 /**
- * woocommerce_before_single_product hook
+ * woocommerce_before_single_product hook.
  *
  * @hooked wc_print_notices - 10
  */
 do_action( 'woocommerce_before_single_product' );
 
 if ( post_password_required() ) {
-    echo get_the_password_form();
-    return;
+	echo get_the_password_form();
+	return;
 }
 ?>
 
@@ -54,15 +61,16 @@ if ( post_password_required() ) {
 
         <?php
         /**
-         * woocommerce_single_product_summary hook
+         * woocommerce_single_product_summary hook.
          *
-         * @hooked woocommerce_template_single_title - 5 - Remove
+         * @hooked woocommerce_template_single_title - 5
          * @hooked woocommerce_template_single_rating - 10
          * @hooked woocommerce_template_single_price - 10
          * @hooked woocommerce_template_single_excerpt - 20
          * @hooked woocommerce_template_single_add_to_cart - 30
-         * @hooked woocommerce_template_single_meta - 40 - Remove
+         * @hooked woocommerce_template_single_meta - 40
          * @hooked woocommerce_template_single_sharing - 50
+         * @hooked WC_Structured_Data::generate_product_data() - 60
          */
         do_action( 'woocommerce_single_product_summary' );
         ?>
