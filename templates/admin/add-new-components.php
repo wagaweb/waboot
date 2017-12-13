be526ee0087f20c84d20fa44186c46557253a863
lostcore@gmail.com <123qw45>
2017-12-13 13:12:43 +0100

    Initial tpl

--- templates/admin/add-new-components.php
+++ templates/admin/add-new-components.php
@@ -1,23 +1,28 @@
+<?php global $page_title; $page_title = __('Add Components','waboot'); ?>
+
+<?php require_once get_template_directory() . '/wbf/admin-header.php'; ?>
+
 <div id="addNewComponents">
     <div class="componentsWrapper">
         <waboot-component v-for="component in available_components" :data="component" :key="component.slug" inline-template>
             <div class="component-card" :data-component="data.slug">
                 <div class="component-card-top">
-                    <div class="name column-name">
-                        <h3>{{ data.title }}</h3>
-                    </div>
-                    <div class="action-links">
-                        <ul class="component-action-buttons">
-                            <li><a href="#" class="button download-now">Download</a></li>
-                        </ul>
+                    <div class="thumbnail column-thumbnail">
+                        <img :src="data.thumbnail" class="component-icon" />
                     </div>
                     <div class="desc column-description">
+                        <h3>{{ data.title }}</h3>
                         <p>
                             {{ data.description }}
                         </p>
                         <p class="authors">
                             <cite>By </cite>{{ data.author }}
                         </p>
+                    </div>
+                    <div class="action-links">
+                        <ul class="component-action-buttons">
+                            <li><a href="#" class="button download-now">Download</a></li>
+                        </ul>
                     </div>
                 </div>
                 <div class="component-card-bottom">
