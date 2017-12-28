cb5d71006803ae921d4d3603fc7aca87090cff6b
LostCore <lostcore@gmail.com>
2017-12-28 15:53:22 +0100

    Updated get_archive_page_title

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -176,58 +176,80 @@ function get_widget_areas(){
 /**
  * Returns the index page title
  *
- * @return string|void
+ * @return string
  */
 function get_index_page_title(){
 	return single_post_title('', false);
 }
 
 /**
- * Returns the appropriate title for the archive page
+ * Returns the appropriate title for the archive page. Clone of get_the_archive_title() with some additions.
  *
  * @return string
  */
 function get_archive_page_title(){
-	global $post;
-	
 	if ( is_category() ) {
-		return single_cat_title('',false);
+		/* translators: Category archive title. 1: Category name */
+		$title = sprintf( __( 'Category: %s' ), single_cat_title( '', false ) );
 	} elseif ( is_tag() ) {
-		return single_tag_title('',false);
-	} elseif ( is_post_type_archive() ) {
-		return post_type_archive_title('', false);
+		/* translators: Tag archive title. 1: Tag name */
+		$title = sprintf( __( 'Tag: %s' ), single_tag_title( '', false ) );
 	} elseif ( is_author() ) {
-		$author_name = get_the_author_meta("display_name",$post->post_author);
-		return sprintf( __( 'Author: %s', 'waboot' ), '<span class="vcard"><a class="url fn n" href="' . get_author_posts_url( $post->post_author ) . '" title="' . esc_attr( $author_name ) . '" rel="me">' . $author_name . '</a></span>' );
-	} elseif ( is_day() ) {
-		return sprintf( __( 'Day: %s', 'waboot' ), '<span>' . get_the_date('', $post->ID) . '</span>' );
-	} elseif ( is_month() ) {
-		return sprintf( __( 'Month: %s', 'waboot' ), '<span>' . get_the_date('F Y', $post->ID ) . '</span>' );
+		/* translators: Author archive title. 1: Author name */
+		$title = sprintf( __( 'Author: %s' ), '<span class="vcard">' . get_the_author() . '</span>' );
 	} elseif ( is_year() ) {
-		return printf( __( 'Year: %s', 'waboot' ), '<span>' . get_the_date('Y', $post->ID ) . '</span>' );
-	} elseif ( is_tax( 'post_format', 'post-format-aside' ) ) {
-		return __( 'Asides', 'waboot' );
-	} elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) {
-		return __( 'Galleries', 'waboot');
-	} elseif ( is_tax( 'post_format', 'post-format-image' ) ) {
-		return __( 'Images', 'waboot');
-	} elseif ( is_tax( 'post_format', 'post-format-video' ) ) {
-		return __( 'Videos', 'waboot' );
-	} elseif ( is_tax( 'post_format', 'post-format-quote' ) ) {
-		return __( 'Quotes', 'waboot' );
-	} elseif ( is_tax( 'post_format', 'post-format-link' ) ) {
-		return __( 'Links', 'waboot' );
-	} elseif ( is_tax( 'post_format', 'post-format-status' ) ) {
-		return __( 'Statuses', 'waboot' );
-	} elseif ( is_tax( 'post_format', 'post-format-audio' ) ) {
-		return __( 'Audios', 'waboot' );
-	} elseif ( is_tax( 'post_format', 'post-format-chat' ) ) {
-		return __( 'Chats', 'waboot' );
+		/* translators: Yearly archive title. 1: Year */
+		$title = sprintf( __( 'Year: %s' ), get_the_date( _x( 'Y', 'yearly archives date format' ) ) );
+	} elseif ( is_month() ) {
+		/* translators: Monthly archive title. 1: Month name and year */
+		$title = sprintf( __( 'Month: %s' ), get_the_date( _x( 'F Y', 'monthly archives date format' ) ) );
+	} elseif ( is_day() ) {
+		/* translators: Daily archive title. 1: Date */
+		$title = sprintf( __( 'Day: %s' ), get_the_date( _x( 'F j, Y', 'daily archives date format' ) ) );
+	} elseif ( is_tax( 'post_format' ) ) {
+		if ( is_tax( 'post_format', 'post-format-aside' ) ) {
+			$title = _x( 'Asides', 'post format archive title' );
+		} elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) {
+			$title = _x( 'Galleries', 'post format archive title' );
+		} elseif ( is_tax( 'post_format', 'post-format-image' ) ) {
+			$title = _x( 'Images', 'post format archive title' );
+		} elseif ( is_tax( 'post_format', 'post-format-video' ) ) {
+			$title = _x( 'Videos', 'post format archive title' );
+		} elseif ( is_tax( 'post_format', 'post-format-quote' ) ) {
+			$title = _x( 'Quotes', 'post format archive title' );
+		} elseif ( is_tax( 'post_format', 'post-format-link' ) ) {
+			$title = _x( 'Links', 'post format archive title' );
+		} elseif ( is_tax( 'post_format', 'post-format-status' ) ) {
+			$title = _x( 'Statuses', 'post format archive title' );
+		} elseif ( is_tax( 'post_format', 'post-format-audio' ) ) {
+			$title = _x( 'Audio', 'post format archive title' );
+		} elseif ( is_tax( 'post_format', 'post-format-chat' ) ) {
+			$title = _x( 'Chats', 'post format archive title' );
+		}
+	} elseif ( is_post_type_archive() ) {
+		/* translators: Post type archive title. 1: Post type name */
+		$title = sprintf( __( 'Archives: %s' ), post_type_archive_title( '', false ) );
+	} elseif ( is_tax() ) {
+		$tax = get_taxonomy( get_queried_object()->taxonomy );
+		/* translators: Taxonomy term archive title. 1: Taxonomy singular name, 2: Current taxonomy term */
+		$title = sprintf( __( '%1$s: %2$s' ), $tax->labels->singular_name, single_term_title( '', false ) );
 	} else {
 		$arch_obj = get_queried_object();
-		if(isset($arch_obj->name)) return $arch_obj->name;
-		return __('Archives', 'waboot');
+		if(isset($arch_obj->name)){
+			$title = $arch_obj->name;
+		}else{
+			$title = __('Archives', 'waboot');
+		}
 	}
+
+	/**
+	 * Filters the archive title.
+	 *
+	 * @since 4.1.0
+	 *
+	 * @param string $title Archive title to be displayed.
+	 */
+	return apply_filters( 'get_the_archive_title', $title );
 }
 
 /**
