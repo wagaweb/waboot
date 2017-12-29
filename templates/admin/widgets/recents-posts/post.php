01213a7bfbcbdc25509533c6f54b0c1eae673b75
clinic <weblab@gmail.com>
2017-12-29 11:32:11 +0100

    Changed recent post visualization for mobile (from col-sm-* to col-xs-*)

--- templates/admin/widgets/recents-posts/post.php
+++ templates/admin/widgets/recents-posts/post.php
@@ -1,9 +1,9 @@
 <article role="article" <?php post_class("recent-post row"); ?>>
 	<?php if(has_post_thumbnail() && $settings['thumb']) : ?>
-		<div class="entry-image col-sm-4 ">
+		<div class="entry-image col-xs-4 ">
 			<a href="<?php the_permalink(); ?>" title="<?php echo $link_title; ?>"><?php echo get_the_post_thumbnail( get_the_ID(), $settings['thumb_size'], ['class' => 'img-responsive'] ); ?></a>
 		</div>
-		<div class="col-sm-8">
+		<div class="col-xs-8">
 			<header>
 				<h4><a href="<?php the_permalink(); ?>" title="<?php echo $link_title; ?>"><?php echo apply_filters("waboot_entry_title_text",get_the_title()); ?></a></h4>
 			</header>
