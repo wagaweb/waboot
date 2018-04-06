cae76fc8bc9bbbd896c38a17bdb994fd1fcc456d
LostCore <lostcore@gmail.com>
2018-04-06 12:36:17 +0200

    Fixes

--- inc/migrations/migration-2.3.2-2.4.0.php
+++ inc/migrations/migration-2.3.2-2.4.0.php
@@ -5,9 +5,20 @@
 namespace Waboot\migrations;
 
 use function Waboot\functions\components\install_remote_component;
+use function Waboot\hooks\do_backups_before_updates;
+use function Waboot\hooks\save_waboot_version_before_update;
 
-add_action('init', function(){
+/*add_action('init', function(){
 	if(!isset($_GET['debug_mig'])) return;
+
+	$params = [
+		'new_version' => '2.4.0',
+		'current_version' => '2.3.2'
+	];
+
+	//Decomment to perform a backup:
+	//save_waboot_version_before_update($params,null);
+	//do_backups_before_updates($params,null);
 
 	\update_option('waboot_updates_backups_components-migrations',[
 		'2.3.2_2.4.0_waboot-child' => [
@@ -20,6 +31,32 @@ add_action('init', function(){
 	\delete_option('waboot-migrations');
 
 	WBF()->get_service_manager()->get_notice_manager()->clear_notices();
+});*/
+
+add_action('init', function(){
+	if(!isset($_GET['waboot_perform_updates'])) return;
+	if(!is_admin()) return;
+
+	$operation = sanitize_text_field($_GET['waboot_perform_updates']);
+
+	$migrations = \get_option('waboot-migrations', []);
+	$current_migration = $migrations['2.3.2-2.4.0'];
+
+	switch($operation){
+		case 'component':
+			$slug = isset($_GET['comp_slug']) ? sanitize_text_field($_GET['comp_slug']) : false;
+			$action = isset($_GET['action']) ? sanitize_text_field($_GET['action']) : false;
+			if($slug){
+				if($action === 'install'){
+					mig_232_240_install_component($slug);
+				}elseif($action === 'dismiss'){
+					$current_migration['installed_component_'.$slug] = true;
+					$migrations['2.3.2-2.4.0'] = $current_migration;
+					\update_option('waboot-migrations',$migrations);
+				}
+			}
+			break;
+	}
 });
 
 add_action('init', function(){
@@ -37,15 +74,12 @@ add_action('init', function(){
 	$complete = false;
 
 	if(is_admin()){
-		$backupped_components_states = \get_option('waboot_updates_backups_components');
-		$current_theme = wp_get_theme();
-		$hash = '2.3.2'.'_'.'2.4.0'.'_'.$current_theme->get_stylesheet();
-		$last_backupped_components_states_update = array_key_exists($hash,$backupped_components_states) ? $backupped_components_states[$hash] : false;
-		if($last_backupped_components_states_update && is_file($last_backupped_components_states_update['file'])){
-			$states = file_get_contents($last_backupped_components_states_update['file']);
+		$components_last_backup = mig_232_240_get_components_states_backup();
+		if($components_last_backup && is_file($components_last_backup['file'])){
+			$states = file_get_contents($components_last_backup['file']);
 			$states = unserialize($states);
 			$notice_msg = 'Waboot 2.4 has removed built-in components. You must reinstall some components:';
-			if(is_array($states) && !empty($states)){
+			if(\is_array($states) && !empty($states)){
 				$states['bootstrap'] = 1; //Force bootstrap
 				$components_to_reinstall = array_filter($states,function($v){
 					return $v === 1;
@@ -79,32 +113,7 @@ add_action('init', function(){
 		$migrations['2.3.2-2.4.0'] = $current_migration;
 		\update_option('waboot-migrations',$migrations);
 	}
-});
-
-add_action('admin_init', function(){
-	if(!isset($_GET['waboot_perform_updates'])) return;
-
-	$operation = sanitize_text_field($_GET['waboot_perform_updates']);
-
-	$migrations = \get_option('waboot-migrations', []);
-	$current_migration = $migrations['2.3.2-2.4.0'];
-
-	switch($operation){
-		case 'component':
-			$slug = isset($_GET['comp_slug']) ? sanitize_text_field($_GET['comp_slug']) : false;
-			$action = isset($_GET['action']) ? sanitize_text_field($_GET['action']) : false;
-			if($slug){
-				if($action === 'install'){
-					mig_232_240_install_component($slug);
-				}elseif($action === 'dismiss'){
-					$current_migration['installed_component_'.$slug] = true;
-					$migrations['2.3.2-2.4.0'] = $current_migration;
-					\update_option('waboot-migrations',$migrations);
-				}
-			}
-			break;
-	}
-});
+},11);
 
 function mig_232_240_install_component($component){
 	$migrations = \get_option('waboot-migrations', []);
@@ -121,4 +130,11 @@ function mig_232_240_install_component($component){
 	}catch (\Exception $e){
 		WBF()->get_service_manager()->get_notice_manager()->add_notice('unable_to_install_component_'.$component,$e->getMessage(),'error','_flash_');
 	}
+}
+
+function mig_232_240_get_components_states_backup(){
+	$backupped_components_states = \get_option('waboot_updates_backups_components');
+	$current_theme = wp_get_theme();
+	$hash = '2.3.2'.'_'.'2.4.0'.'_'.$current_theme->get_stylesheet();
+	return array_key_exists($hash,$backupped_components_states) ? $backupped_components_states[$hash] : false;
 }
