1738f33277e548fbf510e89c5f8f6aab06cdd1dc
123qw45 <lostcore@gmail.com>
2018-04-09 18:05:51 +0200

    Reverted

--- inc/Component.php
+++ inc/Component.php
@@ -2,9 +2,6 @@
 
 namespace Waboot;
 
-use function Waboot\functions\components\get_api_single_component_endpoint;
-use function Waboot\functions\components\request_single_component;
-
 class Component extends \WBF\modules\components\Component{
 	/**
 	 * @var string
@@ -30,7 +27,6 @@ class Component extends \WBF\modules\components\Component{
 	public function __construct( array $component ) {
 		parent::__construct( $component );
 		$this->theme_relative_path = "components/".$this->directory_name;
-		add_action('admin_init', [$this,'enqueue_updates']);
 	}
 
 	public function theme_options($options){
@@ -101,123 +97,5 @@ class Component extends \WBF\modules\components\Component{
 			}
 		}
 		return $p;
-	}
-
-	/**
-	 * Check whether an update is available
-	 *
-	 * @param bool $force
-	 *
-	 * @return bool
-	 * @throws \Exception
-	 */
-	public function check_for_updates($force = false){
-		$can_check_for_update = false;
-		$needs_update = false;
-		if(!$force){
-			$update_check_status = \get_option('waboot_components_update_check_times',[]);
-			if(!array_key_exists($this->name,$update_check_status)){
-				$update_check_status[$this->name] = time();
-				\update_option('waboot_components_update_check_times',$update_check_status);
-				$can_check_for_update = true;
-			}else{
-				$last_check_time = $update_check_status[$this->name];
-				$update_interval = (int) apply_filters('waboot/components/update_check_time_interval', 86400);
-				if( is_int($update_interval) && $update_interval > 0 && (time() - $last_check_time) >= $update_interval ){
-					$update_check_status[$this->name] = time();
-					\update_option('waboot_components_update_check_times',$update_check_status);
-					$can_check_for_update = true;
-				}
-			}
-		}else{
-			$can_check_for_update = true;
-		}
-		if($can_check_for_update) {
-			$update_uri = $this->get_update_uri();
-			if ( $update_uri !== '' ) {
-				try {
-					$data = request_single_component( $this->name, $update_uri );
-					if ( isset( $data['version'] ) ) {
-						$needs_update = version_compare( $this->get_version(), $data['version'], '<' );
-					}
-				} catch ( \Exception $e ) {
-					throw new \Exception($e->getMessage());
-				}
-			}
-		}
-		return $needs_update;
-	}
-
-	/**
-	 * Notify the user of available updates
-	 *
-	 * @hooked 'admin_init'
-	 */
-	public function enqueue_updates(){
-		static $check_done;
-		try{
-			if($check_done !== true){
-				$needs_update = $this->check_for_updates(true);
-				if($needs_update){
-					add_filter('wp_get_update_data', [$this,'alter_update_data'],11,2);
-				}
-			}
-			$check_done = true;
-		}catch(\Exception $e){
-			WBF()->get_service_manager()->get_notice_manager()->add_notice(
-				'unable_to_update_component_' . $this->name,
-				sprintf( __( 'Unable to check for updates of component: %s because of the error: %s' ), $this->name, $e->getMessage() ),
-				'error',
-				'_flash_'
-			);
-		}
-	}
-
-	/**
-	 * Alter WP Update data
-	 *
-	 * @wp_get_update_data by enqueue_updates()
-	 */
-	public function alter_update_data($update_data, $titles){
-		if(!isset($update_data['theme-components'])){
-			$update_data['theme-components'] = 1;
-		}else{
-			$update_data['theme-components']++;
-		}
-		$update_data['counts']['total'] = $update_data['counts']['total'] + 1;
-
-		$update_data['title'] = $titles ? esc_attr( implode( ', ', $titles ) ) : '';
-
-		$update_data['title'] = $update_data['title'].' , '.sprintf( _n( '%d Component Update', '%d Component Updates', $update_data['theme-components'] ), $update_data['theme-components'] );
-
-		return $update_data;
-	}
-
-	/**
-	 * Set the component update uri
-	 *
-	 * @param $uri
-	 */
-	public function set_update_uri($uri){
-		$this->update_uri = $uri;
-	}
-
-	/**
-	 * Get the component update uri
-	 *
-	 * @return string
-	 */
-	public function get_update_uri(){
-		if(!isset($this->update_uri)){
-			$component_data = get_file_data( $this->file, ['UpdateURI' => 'Update URI', 'Author' => 'Author', 'AuthorURI' => 'Author URI'] );
-			if(!isset($component_data['UpdateURI']) || $component_data['UpdateURI'] === ''){
-				if(isset($component_data['Author']) && strpos($component_data['Author'],'waga') !== false){
-					$this->set_update_uri(get_api_single_component_endpoint($this->name));
-				}
-			}else{
-				$this->set_update_uri('');
-			}
-		}
-		return $this->update_uri;
 	}
 }
