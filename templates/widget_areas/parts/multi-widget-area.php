ce919efb56af293b69be628eb86f97614fd50de5
fabosway <fabio@waga.it>
2018-04-19 17:16:24 +0200

    Fixed Multi Widget Area Template

--- templates/widget_areas/parts/multi-widget-area.php
+++ templates/widget_areas/parts/multi-widget-area.php
@@ -1,11 +1,11 @@
-<div class="<?php echo $widget_area_prefix; ?>-sidebar-row">
-	<?php do_action("waboot/widget_area/before"); ?>
-	<?php do_action("waboot/widget_area/{$widget_area_prefix}/before"); ?>
-	<?php for($i=1;$i<=$widget_count;$i++): ?>
-		<aside id="<?php echo $widget_area_prefix; ?>-sidebar-<?php echo $i; ?>" class="sidebar widget <?php echo $sidebar_class; ?>">
-			<?php dynamic_sidebar( $widget_area_prefix.'-'.$i ); ?>
-		</aside>
-	<?php endfor; ?>
-	<?php do_action("waboot/widget_area/after"); ?>
-	<?php do_action("waboot/widget_area/{$widget_area_prefix}/after"); ?>
+<div class="<?php echo $widget_area_prefix; ?>__row">
+    <?php do_action("waboot/widget_area/before"); ?>
+    <?php do_action("waboot/widget_area/{$widget_area_prefix}/before"); ?>
+    <?php for($i=1;$i<=$widget_count;$i++): ?>
+        <aside class="<?php echo $widget_area_prefix; ?>__aside--<?php echo $i; ?> widget">
+            <?php dynamic_sidebar( $widget_area_prefix.'-'.$i ); ?>
+        </aside>
+    <?php endfor; ?>
+    <?php do_action("waboot/widget_area/after"); ?>
+    <?php do_action("waboot/widget_area/{$widget_area_prefix}/after"); ?>
 </div>
