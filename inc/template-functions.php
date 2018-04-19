035f14f9cc13e8d46fe91b6a48f87b77ee6fd7d8
LostCore <lostcore@gmail.com>
2018-04-19 15:30:47 +0200

    suffix removed

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -183,20 +183,20 @@ function get_index_page_title(){
 }
 
 /**
- * Returns the appropriate title for the archive page. Clone of get_the_archive_title() with some additions.
+ * Returns the appropriate title for the archive page. Clone of get_the_archive_title() with some editing (eg: some suffix has been removed).
  *
  * @return string
  */
 function get_archive_page_title(){
 	if ( is_category() ) {
 		/* translators: Category archive title. 1: Category name */
-		$title = sprintf( __( '%s' ), single_cat_title( '', false ) );
+		$title = sprintf( '%s', single_cat_title( '', false ) );
 	} elseif ( is_tag() ) {
 		/* translators: Tag archive title. 1: Tag name */
-		$title = sprintf( __( '%s' ), single_tag_title( '', false ) );
+		$title = sprintf( '%s', single_tag_title( '', false ) );
 	} elseif ( is_author() ) {
 		/* translators: Author archive title. 1: Author name */
-		$title = sprintf( __( '%s' ), '<span class="vcard">' . get_the_author() . '</span>' );
+		$title = sprintf( '%s', '<span class="vcard">' . get_the_author() . '</span>' );
 	} elseif ( is_year() ) {
 		/* translators: Yearly archive title. 1: Year */
 		$title = sprintf( __( 'Year: %s' ), get_the_date( _x( 'Y', 'yearly archives date format' ) ) );
@@ -228,11 +228,10 @@ function get_archive_page_title(){
 		}
 	} elseif ( is_post_type_archive() ) {
 		/* translators: Post type archive title. 1: Post type name */
-		$title = sprintf( __( '%s' ), post_type_archive_title( '', false ) );
+		$title = sprintf( '%s', post_type_archive_title( '', false ) );
 	} elseif ( is_tax() ) {
-		$tax = get_taxonomy( get_queried_object()->taxonomy );
 		/* translators: Taxonomy term archive title. 1: Taxonomy singular name, 2: Current taxonomy term */
-		$title = sprintf( __( '%1$s' ), single_term_title( '', false ) );
+		$title = sprintf( '%1$s', single_term_title( '', false ) );
 	} else {
 		$arch_obj = get_queried_object();
 		if(isset($arch_obj->name)){
