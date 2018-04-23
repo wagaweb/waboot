566f5e90210e8e242b4d449e4be57fed7e430a77
Riccardo D'Angelo <riccardo@waga.it>
2018-04-23 11:17:56 +0200

    Generators refactoring

--- inc/generators/layout-one.php
+++ inc/generators/layout-one.php
@@ -3,7 +3,7 @@
 require_once 'WBFInstaller.php';
 require_once 'WabootGenerator.php';
 
-class BootstrapGenerator extends WabootGenerator {
+class LayoutOneGenerator extends WabootGenerator {
 	use WBFInstaller;
 
 	const DOWNLOAD_URL = 'http://update.waboot.org/resource/get/plugin/wbf';
--- inc/generators/layout-two.php
+++ inc/generators/layout-two.php
@@ -3,7 +3,7 @@
 require_once 'WBFInstaller.php';
 require_once 'WabootGenerator.php';
 
-class ClassicGenerator extends WabootGenerator {
+class LayoutTwoGenerator extends WabootGenerator {
 	use WBFInstaller;
 
 	const DOWNLOAD_URL = 'http://update.waboot.org/resource/get/plugin/wbf';
