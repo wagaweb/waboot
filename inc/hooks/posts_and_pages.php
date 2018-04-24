83b4e5495a32db483af85b9e7b963040f2618a57
LostCore <lostcore@gmail.com>
2018-04-24 16:55:08 +0200

    Classes

--- inc/hooks/posts_and_pages.php
+++ inc/hooks/posts_and_pages.php
@@ -268,7 +268,7 @@ function display_title_wrapper_start(){
 	}
 
 	if($can_display){
-		echo '<div class="'.WabootLayout()->get_grid_class('container').'">';
+		echo '<div class="entry__wrapper">';
 	}
 }
 
