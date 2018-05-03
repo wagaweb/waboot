9b9937d3701bf49ae58f77d8573f252339b206ef
LostCore <lostcore@gmail.com>
2018-05-03 16:43:01 +0200

    New classes

--- templates/view-parts/archive-title.php
+++ templates/view-parts/archive-title.php
@@ -1,5 +1,5 @@
-<header class="entry__header">
+<header class="entry__header archive__header">
     <?php do_action('waboot/layout/archive/page_title/before'); ?>
-    <?php \Waboot\template_tags\wrapped_title('<h1 class="entry__title archive__title">','</h1>',$title); ?>
+    <?php \Waboot\template_tags\wrapped_title('<h1 class="entry__title">','</h1>',$title); ?>
     <?php do_action('waboot/layout/archive/page_title/after'); ?>
 </header>
