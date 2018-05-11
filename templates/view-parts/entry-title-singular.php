3f2600106064b20345c0e14eb87be66e617e6489
fabosway <fabio@waga.it>
2018-05-11 17:03:28 +0200

    Fix CSS Responsive Table Fix Entry Title Singular Classes

--- templates/view-parts/entry-title-singular.php
+++ templates/view-parts/entry-title-singular.php
@@ -1,4 +1,4 @@
-<header class="entry__header main__title <?php if(isset($title_position)): ?>main__title--"<?php echo $title_position; ?><?php endif; ?>>
+<header class="entry__header main__title<?php if(isset($title_position)): ?> main__title--<?php echo $title_position; ?><?php endif; ?>">
     <?php do_action('waboot/layout/singular/page_title/before'); ?>
     <?php \Waboot\template_tags\wrapped_title('<h1 class="entry__title">','</h1>',$title); ?>
     <?php do_action('waboot/layout/singular/page_title/after'); ?>
