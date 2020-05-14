6ecd5a850b1bde3407d269dc9983cbad9f1c338e
LostCore <lostcore@gmail.com>
2020-05-14 16:19:41 +0200

    edited hooks in layout.php

--- inc/hooks/layout.php
+++ inc/hooks/layout.php
@@ -4,12 +4,21 @@ namespace Waboot\inc\hooks;
 
 use function Waboot\inc\core\Waboot;
 
-function displayHeader(){
+add_action('waboot/layout/header', function(){
     Waboot()->renderView('templates/view-parts/main-header.php');
-}
-add_action('waboot/layout/header', __NAMESPACE__."\\displayHeader");
+});
 
-function displayFooter(){
+add_action('waboot/layout/footer', function(){
     Waboot()->renderView('templates/view-parts/main-footer.php');
-}
-add_action('waboot/layout/footer', __NAMESPACE__."\\displayFooter");
+});
+
+/*
+ * Hide Title in Specific Page
+ *
+add_filter('waboot/main/title/display_flag', function($can_display_title){
+    if(is_front_page()){
+        return false;
+    }
+    return $can_display_title;
+},5,3);
+*/
