a68e959b722c779c9bb30703a304411dcc1578d7
LostCore <lostcore@gmail.com>
2020-11-24 16:17:03 +0100

    Security update

--- inc/hooks/hooks.php
+++ inc/hooks/hooks.php
@@ -5,6 +5,36 @@ namespace Waboot\inc\hooks;
 use Waboot\inc\core\utils\Utilities;
 
 /**
+ * Remove user list endpoint from rest api
+ * @see: https://hackertarget.com/wordpress-user-enumeration/
+ */
+add_filter('rest_endpoints', function($endpoints){
+    if(isset($endpoints['/wp/v2/users'])){
+        unset($endpoints['/wp/v2/users']);
+    }
+    if(isset($endpoints['/wp/v2/users/(?P<id>[\d]+)'])){
+        unset($endpoints['/wp/v2/users/(?P<id>[\d]+)']);
+    }
+    return $endpoints;
+});
+
+/**
+ * Only Allow logged-in rest access to users
+ * @see: https://hackertarget.com/wordpress-user-enumeration/
+ */
+/*add_action('rest_authentication_errors', function(){
+    if((strpos($_SERVER['REQUEST_URI'], "users") !== false) || ( isset($_REQUEST['rest_route']) && (strpos($_REQUEST['rest_route'], "users") !== false) )){
+        if(!is_user_logged_in()){
+            return new \WP_Error(
+                'rest_cannot_access',
+                esc_html__('Only authenticated users can access the User endpoint REST API.', 'waboot'),
+                array('status' => rest_authorization_required_code())
+            );
+        }
+    }
+});*/
+
+/**
  * Add header metas
  */
 function addHeaderMetas(){
