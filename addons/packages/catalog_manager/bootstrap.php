bc99e643f88a2847eb07b433b11f8c0da93e35c3
LostCore <lostcore@gmail.com>
2021-01-05 13:19:47 +0100

    modulo catalog manager

---
+++ addons/packages/catalog_manager/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Waboot\addons\packages\catalog_manager;
+
+if(\is_file(__DIR__.'/waga-woocommerce-csv-cli-importer/src/command.php')){
+    require_once __DIR__.'/waga-woocommerce-csv-cli-importer/src/command.php';
+}
