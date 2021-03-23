24cfa79413508de652d57ed90b7b30cc9324492d
fabosway <fabosway@gmail.com>
2021-03-23 12:33:20 +0100

    Fix Traduzioni

--- templates/parts/content-single.php
+++ templates/parts/content-single.php
@@ -4,7 +4,7 @@
             <?php
             $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' );
             echo '<a href="' . $large_image_url[0] . '" title="' . the_title_attribute( 'echo=0' ) . '">';
-            echo get_the_post_thumbnail( $post->ID, 'large', array( 'class' => 'img-responsive', 'title' => "" ) );
+            echo get_the_post_thumbnail( get_the_id(), 'large', array( 'class' => 'img-responsive', 'title' => "" ) );
             echo '</a>';
             ?>
         </figure>
