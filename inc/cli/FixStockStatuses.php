09ec5074707324c8792bae7505c4cdc51cb9217e
LostCore <lostcore@gmail.com>
2021-04-29 12:58:49 +0200

    FixStockStatuses.php

--- inc/cli/FixStockStatuses.php
+++ inc/cli/FixStockStatuses.php
@@ -34,9 +34,12 @@ class FixStockStatuses extends ParseAndSaveProducts
         add_action('wawoo/cli/parse-and-save-products/post-save/single', static function(\WC_Product $product, ParseAndSaveProducts $class){
             $qtyMeta = get_post_meta($product->get_id(),'_stock', true);
             $statusMeta = get_post_meta($product->get_id(),'_stock_status', true);
+            $class->log('- _stock: '.$qtyMeta);
+            $class->log('- _stock_status: '.$statusMeta);
             $visibilityTerms = wp_get_post_terms($product->get_id(),'product_visibility');
             $visibilityTermsSlugs = wp_list_pluck($visibilityTerms,'slug');
             $managingStock = $product->managing_stock();
+            $class->log('- Manage stock? '.(int) $managingStock);
             if(!$managingStock){
                 return;
             }
@@ -47,7 +50,7 @@ class FixStockStatuses extends ParseAndSaveProducts
                  * If we have a variable product, its stock status must take into account
                  * the variations quantities
                  */
-                $class->log('- Checking variations');
+                $class->log('-- Checking variations');
                 $variations = $class->getProductVariations($product->get_id(),'object');
                 $totalVariationsQty = 0;
                 $atLeastOneVariationIsStockManaging = false;
@@ -61,12 +64,12 @@ class FixStockStatuses extends ParseAndSaveProducts
                     $totalVariationsQty += $vQty;
                 }
                 if($atLeastOneVariationIsStockManaging){
-                    $class->log('- Variations with managing quantity flag total quantity: '.$totalVariationsQty);
+                    $class->log('--- Variations with managing quantity flag total quantity: '.$totalVariationsQty);
                     $realStatus = $totalVariationsQty > 0 ? 'instock' : 'outofstock';
                 }
             }
             if($realStatus !== $statusMeta){
-                $class->log('- Set "_stock_status" to: '.$realStatus);
+                $class->log('-- Set "_stock_status" to: '.$realStatus);
                 update_post_meta($product->get_id(),'_stock_status',$realStatus);
             }
             if(!$product instanceof \WC_Product_Variation){
@@ -75,10 +78,10 @@ class FixStockStatuses extends ParseAndSaveProducts
                  * out-of-stock products.
                  */
                 if($realStatus === 'instock' && \in_array('outofstock',$visibilityTermsSlugs,true)){
-                    $class->log('- Removing "outofstock" product_visibility term');
+                    $class->log('-- Removing "outofstock" product_visibility term');
                     wp_remove_object_terms($product->get_id(),'outofstock','product_visibility');
                 }elseif($realStatus === 'outofstock' && !\in_array('outofstock',$visibilityTermsSlugs,true)){
-                    $class->log('- Adding "outofstock" product_visibility term');
+                    $class->log('-- Adding "outofstock" product_visibility term');
                     wp_add_object_terms($product->get_id(),'outofstock','product_visibility');
                 }
             }
