b88da73a78110433aeb098c0d4b9255391c2984c
LostCore <lostcore@gmail.com>
2021-06-09 13:17:38 +0200

    more woocommerce helpers

--- inc/woocommerce-helpers.php
+++ inc/woocommerce-helpers.php
@@ -121,8 +121,8 @@ function getProductSalePercentage(\WC_Product $product, $round = true, $roundPre
 }
 
 /**
- * Get a custom field from the provided product. If the product is a variation, the parent will be used as source
- * if the field doesn't exists.
+ * Get a custom field from the provided product. If the product is a variation and doesn't have the field,
+ * the parent will be used as source.
  *
  * @param \WC_Product $product
  * @param string $fieldKey
@@ -144,4 +144,140 @@ function getHierarchicalCustomFieldFromProduct(\WC_Product $product, string $fie
         $fieldValue = $default;
     }
     return $fieldValue;
+}
+
+/**
+ * Get the product image data (url, with, height)
+ * @see: \WC_Product::get_image()
+ *
+ * @param \WC_Product $product
+ * @param string $size
+ * @param array $attr
+ * @param bool $placeholder
+ * @return array
+ */
+function getWCProductImageData( \WC_Product $product, $size = 'woocommerce_thumbnail', $attr = array(), $placeholder = true): array {
+    $image = [];
+    if($product->get_image_id()){
+        $image = wp_get_attachment_image_src($product->get_image_id(), $size, false);
+    }elseif($product->get_parent_id()){
+        $parent_product = wc_get_product($product->get_parent_id());
+        if ( $parent_product ) {
+            $image = getWCProductImageData($parent_product, $size, $attr, $placeholder);
+        }
+    }
+
+    if(!$image && $placeholder){
+        $image = wc_placeholder_img_src($size);
+    }
+
+    if(!\is_array($image)){
+        $image = [];
+    }
+
+    return $image;
+}
+
+/**
+ * Get the data to render the mini cart. Useful to use in ajax calls.
+ * @see: /woocommerce/templates/cart/mini-cart.php
+ *
+ * @return array
+ */
+function getMiniCartData(): array {
+    if(!function_exists('WC')){
+        return [];
+    }
+    if(WC()->cart->is_empty()){
+        return [
+            'no_items_message' => esc_html__('No products in the cart.', 'woocommerce')
+        ];
+    }
+    $resultData = [];
+    foreach ( WC()->cart->get_cart() as $cartItemKey => $cartItem ){
+        $product = apply_filters( 'woocommerce_cart_item_product', $cartItem['data'], $cartItem, $cartItemKey );
+        $productID = apply_filters( 'woocommerce_cart_item_product_id', $cartItem['product_id'], $cartItem, $cartItemKey );
+        if ( $product && $product->exists() && $cartItem['quantity'] > 0 && apply_filters( 'woocommerce_widget_cart_item_visible', true, $cartItem, $cartItemKey ) ) {
+            $productName = apply_filters( 'woocommerce_cart_item_name', $product->get_name(), $cartItem, $cartItemKey );
+            //$thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $product->get_image(), $cartItem, $cartItemKey );
+            $thumbnail = getWCProductImageData($product);
+            $productPrice = apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $product ), $cartItem, $cartItemKey );
+            $productPrice = strip_tags($productPrice);
+            //$productPermalink = apply_filters( 'woocommerce_cart_item_permalink', $product->is_visible() ? $product->get_permalink( $cartItem ) : '', $cartItem, $cartItemKey );
+            $productPermalink = $product->is_visible() ? $product->get_permalink( $cartItem ) : '';
+            $itemListClass = esc_attr( apply_filters( 'woocommerce_mini_cart_item_class', 'mini_cart_item', $cartItem, $cartItemKey ) );
+            $removeLink = apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+                'woocommerce_cart_item_remove_link',
+                sprintf(
+                    '<a href="%s" class="remove remove_from_cart_button" aria-label="%s" data-product_id="%s" data-cart_item_key="%s" data-product_sku="%s">&times;</a>',
+                    esc_url( wc_get_cart_remove_url( $cartItemKey ) ),
+                    esc_attr__( 'Remove this item', 'woocommerce' ),
+                    esc_attr( $productID ),
+                    esc_attr( $cartItemKey ),
+                    esc_attr( $product->get_sku() )
+                ),
+                $cartItemKey
+            );
+            $itemData = [];
+            /*
+             * Getting item cart additional data
+             * @see: wc_get_formatted_cart_item_data()
+             */
+            if ( $cartItem['data']->is_type( 'variation' ) && is_array( $cartItem['variation'] ) ) {
+                foreach ( $cartItem['variation'] as $name => $value ) {
+                    $taxonomy = wc_attribute_taxonomy_name( str_replace( 'attribute_pa_', '', urldecode( $name ) ) );
+                    if ( taxonomy_exists( $taxonomy ) ) {
+                        $term = get_term_by( 'slug', $value, $taxonomy );
+                        if ( ! is_wp_error( $term ) && $term && $term->name ) {
+                            $value = $term->name;
+                        }
+                        $label = wc_attribute_label( $taxonomy );
+                    }else {
+                        // If this is a custom option slug, get the options name.
+                        $value = apply_filters( 'woocommerce_variation_option_name', $value, null, $taxonomy, $cartItem['data'] );
+                        $label = wc_attribute_label( str_replace( 'attribute_', '', $name ), $cartItem['data'] );
+                    }
+                    if ( '' === $value || wc_is_attribute_in_product_name( $value, $cartItem['data']->get_name() ) ) {
+                        continue;
+                    }
+                    $itemData[] = [
+                        'key' => $label,
+                        'value' => $value,
+                    ];
+                }
+            }
+            $itemData = apply_filters( 'woocommerce_get_item_data', $itemData, $cartItem );
+            foreach ( $itemData as $key => $data ) {
+                // Set hidden to true to not display meta on cart.
+                if (!empty( $data['hidden'])){
+                    unset($itemData[$key]);
+                    continue;
+                }
+                $itemData[$key]['key'] = ! empty($data['key']) ? $data['key'] : $data['name'];
+                $itemData[$key]['display'] = ! empty($data['display']) ? $data['display'] : $data['value'];
+            }
+            /*
+             * Finalize the item
+             */
+            $resultData['items'][] = [
+                'key' => $cartItemKey,
+                'product_name' => $productName,
+                'thumbnail' => $thumbnail,
+                'price' => $productPrice,
+                'permalink' => $productPermalink,
+                'list_element_class' => $itemListClass,
+                'remove_link' => $removeLink,
+                'data' => $itemData
+            ];
+        }
+    }
+    $subtotal = WC()->cart->get_cart_subtotal();
+    $subtotal = strip_tags($subtotal);
+    $resultData['subtotal'] = $subtotal;
+    $resultData['view_cart_url'] = esc_url(wc_get_cart_url());
+    $resultData['view_cart_label'] = esc_html__('View cart', 'woocommerce');
+    $resultData['goto_checkout_url'] = esc_url(wc_get_checkout_url());
+    $resultData['goto_checkout_label'] = esc_html__('Checkout', 'woocommerce');
+    $resultData = apply_filters('waboot/woocommerce/cart_data', $resultData);
+    return $resultData;
 }
