0c70763cc3c0e8f10a73edbf3e3c6c18e6b6a83f
Damiano Todeschini <damiano@waga.it>
2022-01-10 10:17:25 +0100

    article footer icons improved

--- templates/view-parts/entry-author.php
+++ templates/view-parts/entry-author.php
@@ -1,17 +1,20 @@
 <span class="byline">
-	<svg
-		width="18"
-		height="18"
-		fill="none"
-		stroke="currentColor"
-		stroke-width="1.5"
-		stroke-linecap="round"
-		stroke-linejoin="round"
-	>
-		<use href="<?php echo get_template_directory_uri() ?>/assets/images/default/icons/feather-sprite.svg#user"/>
-	</svg>
+	
 
 	<span class="author vcard">
-		<a class="url fn n" href="<?php echo $author_url; ?>" title="<?php echo $author_title; ?>" rel="author"><?php echo $author_name; ?></a>
+		<a class="url fn n" href="<?php echo $author_url; ?>" title="<?php echo $author_title; ?>" rel="author">
+			<svg
+			width="18"
+			height="18"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="1.5"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		>
+			<use href="<?php echo get_template_directory_uri() ?>/assets/images/default/icons/feather-sprite.svg#user"/>
+		</svg>
+		
+		<?php echo $author_name; ?></a>
 	</span>
 </span>
