221e6eb0a9e1e0e8e7a312d6369a7aa03835a199
Damiano Todeschini <damiano@waga.it>
2022-01-10 13:12:19 +0100

    fixed overflow on mobile viewport

--- templates/single.php
+++ templates/single.php
@@ -1,15 +1,13 @@
-<div class="container--narrow">
-    <?php while ( have_posts() ) : the_post(); $pt = get_post_type(); ?>
-        <?php if(locate_template('templates/parts/content-' .$pt. '.php', false, false) != '') : ?>
-            <?php get_template_part('templates/parts/content', $pt); ?>
-        <?php else: ?>
-            <?php get_template_part('templates/parts/content', 'single'); ?>
-        <?php endif; ?>
-        <?php
-        if(comments_open() || '0' != get_comments_number()){
-            comments_template('/templates/comments.php',true);
-        }
-        ?>
-    <?php endwhile; ?>
-    <?php \Waboot\inc\renderPostNavigation('nav-below'); ?>
-</div>
+<?php while ( have_posts() ) : the_post(); $pt = get_post_type(); ?>
+    <?php if(locate_template('templates/parts/content-' .$pt. '.php', false, false) != '') : ?>
+        <?php get_template_part('templates/parts/content', $pt); ?>
+    <?php else: ?>
+        <?php get_template_part('templates/parts/content', 'single'); ?>
+    <?php endif; ?>
+    <?php
+    if(comments_open() || '0' != get_comments_number()){
+        comments_template('/templates/comments.php',true);
+    }
+    ?>
+<?php endwhile; ?>
+<?php \Waboot\inc\renderPostNavigation('nav-below'); ?>
