de87c787ac8ca095bd346b514fa2b3bec682a792
Damiano Todeschini <damiano@waga.it>
2022-01-11 18:19:29 +0100

    button outline styles defined page title improved

--- templates/view-parts/main-title.php
+++ templates/view-parts/main-title.php
@@ -1,5 +1,7 @@
-<div class="main__title <?php echo $classes; ?>">
-    <?php do_action('waboot/layout/title/before'); ?>
-    <?php \Waboot\inc\wrappedTitle('<h1>','</h1>',$title); ?>
-    <?php do_action('waboot/layout/title/after'); ?>
-</div>
+<?php if( !is_front_page() ) : ?>
+    <div class="main__title <?php echo $classes; ?>">
+        <?php do_action('waboot/layout/title/before'); ?>
+        <?php \Waboot\inc\wrappedTitle('<h1>','</h1>',$title); ?>
+        <?php do_action('waboot/layout/title/after'); ?>
+    </div>
+<?php endif; ?>
