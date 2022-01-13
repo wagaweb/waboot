43c2991234ec6944b806e1d4c718bace491d2c91
Damiano Todeschini <damiano@waga.it>
2022-01-13 15:05:53 +0100

    breadcrumbs implemented

--- templates/view-parts/main-title.php
+++ templates/view-parts/main-title.php
@@ -1,5 +1,11 @@
 <?php if( !is_front_page() ) : ?>
     <div class="main__title <?php echo $classes; ?>">
+        <?php
+        if ( function_exists('yoast_breadcrumb') ) {
+            yoast_breadcrumb( '<nav class="breadcrumbs">','</nav>' );
+            }
+        ?>
+
         <?php do_action('waboot/layout/title/before'); ?>
         <?php \Waboot\inc\wrappedTitle('<h1>','</h1>',$title); ?>
         <?php do_action('waboot/layout/title/after'); ?>
