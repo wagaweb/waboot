5e641cca34f16b72b2eb17b3ec417bc99d457f5e
LostCore <lostcore@gmail.com>
2022-01-27 11:14:53 +0100

    loadDependencies()

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -9,7 +9,7 @@ function initWaboot(){
         $parentPath = get_template_directory();
         require_once $parentPath.'/inc/template-functions.php';
         require_once $parentPath.'/inc/core/template-functions.php';
-        Waboot()->loadDependecies();
+        Waboot()->loadDependencies();
     }catch (\Exception $e){
         trigger_error($e->getMessage(), E_USER_ERROR);
     }
