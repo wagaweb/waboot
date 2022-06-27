a8d801c7d393132477509c7381f964a33e0272a4
LostCore <lostcore@gmail.com>
2022-06-27 18:08:40 +0200

    Posts.php utility

---
+++ inc/core/utils/Posts.php
@@ -0,0 +1,113 @@
+<?php
+namespace Waboot\inc\core\utils;
+
+class Posts {
+    /**
+     * @param int $pageId
+     * @return false|int
+     */
+    public static function getPostTopAncestorId(int $pageId)
+    {
+        $ancestors = get_post_ancestors($pageId);
+        if(!\is_array($ancestors) || count($ancestors) === 0){
+            return false;
+        }
+        $topLevelAncestorId = false;
+        foreach ($ancestors as $ancestorId) {
+            if(wp_get_post_parent_id($ancestorId) > 0){
+                continue;
+            }
+            $topLevelAncestorId = $ancestorId;
+        }
+        return $topLevelAncestorId;
+    }
+
+    /**
+     * @param $pageId
+     * @return bool
+     */
+    public static function pageHasChildren($pageId): bool
+    {
+        return count(get_pages(['child_of' => $pageId])) > 0;
+    }
+
+    /**
+     * Get the full page hierarchy starting from any page in the hierarchy
+     * @param int $startingPostId
+     * @param bool $includeSelf
+     * @param bool $generateFullTree
+     * @return array
+     */
+    public static function getPostsTreeByPostId(int $startingPostId, $includeSelf = true, $generateFullTree = true): array
+    {
+        $currentQueriedObjectId = get_queried_object_id();
+        $currentObjectAncestorIds = [];
+        if(is_page($currentQueriedObjectId)){
+            $currentObjectAncestorIds = get_post_ancestors($startingPostId);
+        }
+
+        if($generateFullTree && self::hasPostParent($startingPostId)){
+            $mainParentId = self::getPostTopAncestorId($startingPostId);
+        }else{
+            $mainParentId = $startingPostId;
+        }
+
+        $generateTree = static function($parentId,$currentLevel = 1) use (&$generateTree,$currentObjectAncestorIds,$startingPostId){
+            $pageTree = [];
+            $pages = get_pages([
+                'child_of' => $parentId,
+                'sort_column' => 'menu_order'
+            ]);
+            foreach ($pages as $page){
+                if($page->post_parent !== $parentId) {
+                    continue;
+                }
+                $newEl = [
+                    'post' => $page,
+                    'attr' => [
+                        'active' => \in_array($page->ID,$currentObjectAncestorIds,true) || $page->ID === $startingPostId,
+                        'current' => $page->ID === $startingPostId
+                    ],
+                    'children' => []
+                ];
+                if(self::pageHasChildren($page->ID)){
+                    $newEl['children'] = $generateTree($page->ID, $currentLevel + 1); //Recursion
+                }
+                $pageTree[] = $newEl;
+            }
+            return $pageTree;
+        };
+
+        $tree = $generateTree($mainParentId);
+        if(!\is_array($tree)){
+            $tree = [];
+        }
+        if(!$includeSelf){
+            return $tree;
+        }
+        return [
+            [
+                'post' => get_post($mainParentId),
+                'attr' => [
+                    'active' => \in_array($mainParentId,$currentObjectAncestorIds,true) || $mainParentId === $startingPostId,
+                    'current' => $mainParentId === $startingPostId
+                ],
+                'children' => $tree
+            ]
+        ];
+    }
+
+    /**
+     * @param int $postId
+     * @return bool
+     */
+    public static function hasPostParent(int $postId): bool
+    {
+        if(\function_exists('\has_post_parent')){
+            return has_post_parent($postId);
+        }
+        $wpPost = get_post($postId);
+        $wpPostParent = !empty($wpPost->post_parent) ? get_post($wpPost->post_parent) : null;
+        return (bool) $wpPostParent;
+    }
+}
