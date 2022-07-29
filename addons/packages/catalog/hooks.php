1a14dee8c250d275b966948e6abbd6f24fec0109
fabosway <fabosway@gmail.com>
2022-07-29 16:47:00 +0200

    Fix Gutenberg Products Blocks

--- addons/packages/catalog/hooks.php
+++ addons/packages/catalog/hooks.php
@@ -18,29 +18,30 @@ add_filter(
     2
 );
 
-add_filter(
-    'woocommerce_blocks_product_grid_item_html',
-    function (string $html, object $data, \WC_Product $product): string {
-        if (!defined('WB_CATALOG_BASEURL')) {
-            return $html;
-        }
-
-        return sprintf(
-            '<li>%s</li>',
-            renderSimpleCatalog(
-                [
-                    'productIds' => [$product->get_id()],
-                    'columns' => 1,
-                    'showAddToCartBtn' => false,
-                    'gtag' => [
-                        'enabled' => false,
-                        'listName' => \Waboot\addons\packages\catalog\getGtagListName(),
-                        'brandFallback' => get_bloginfo('name'),
-                    ],
-                ]
-            )
-        );
-    },
-    20,
-    3
-);
+add_filter('init', function(){
+    if(!defined('WB_CATALOG_BASEURL') || strpos($_SERVER[ 'REQUEST_URI' ], '/wp-json/') !== false){
+        return;
+    }
+    add_filter(
+        'woocommerce_blocks_product_grid_item_html',
+        function (string $html, object $data, \WC_Product $product): string {
+            return sprintf(
+                '<li>%s</li>',
+                renderSimpleCatalog(
+                    [
+                        'productIds' => [$product->get_id()],
+                        'columns' => 1,
+                        'showAddToCartBtn' => false,
+                        'gtag' => [
+                            'enabled' => false,
+                            'listName' => \Waboot\addons\packages\catalog\getGtagListName(),
+                            'brandFallback' => get_bloginfo('name'),
+                        ],
+                    ]
+                )
+            );
+        },
+        20,
+        3
+    );
+});
