7641716400c693c0cb7f2026867d48493de6c7f2
Lorenzo Polverelli <lorenzo.polverelli13@gmail.com>
2022-10-28 14:22:28 +0200

    refactored catalog init

--- addons/packages/catalog/assets/src/main.ts
+++ addons/packages/catalog/assets/src/main.ts
@@ -7,50 +7,51 @@ import messages, { AvailableLanguages } from '@/i18n';
 
 import './sass/main.scss';
 
-(() => {
-  for (const entry of document.querySelectorAll('.vue-catalog')) {
-    const rawConfig = JSON.parse(
-      entry.getAttribute('catalog-config') ?? 'null',
-    );
-    if (rawConfig === null) {
-      console.error('catalog-config property not specified');
-
-      continue;
-    }
-
-    const config = new CatalogConfig(rawConfig);
-    const app = createApp(Catalog, { config });
-
-    const i18n = createI18n({
-      locale: config.language,
-      fallbackLocale: AvailableLanguages.itIT,
-      messages,
-    });
-    app.use(i18n);
-
-    app.mount(entry);
+function initCatalog(entry: Element): void {
+  const rawConfig = JSON.parse(entry.getAttribute('catalog-config') ?? 'null');
+  if (rawConfig === null) {
+    console.error('catalog-config property not specified');
+    return;
   }
 
-  for (const entry of document.querySelectorAll('.vue-simple-catalog')) {
-    const rawConfig = JSON.parse(
-      entry.getAttribute('catalog-config') ?? 'null',
-    );
-    if (rawConfig === null) {
-      console.error('catalog-config property not specified');
+  const config = new CatalogConfig(rawConfig);
+  const app = createApp(Catalog, { config });
 
-      continue;
-    }
+  const i18n = createI18n({
+    locale: config.language,
+    fallbackLocale: AvailableLanguages.itIT,
+    messages,
+  });
+  app.use(i18n);
 
-    const config = new CatalogConfig(rawConfig);
-    const app = createApp(SimpleCatalog, { config });
+  app.mount(entry);
+}
 
-    const i18n = createI18n({
-      locale: config.language,
-      fallbackLocale: AvailableLanguages.itIT,
-      messages,
-    });
-    app.use(i18n);
-
-    app.mount(entry);
+function initSimpleCatalog(entry: Element): void {
+  const rawConfig = JSON.parse(entry.getAttribute('catalog-config') ?? 'null');
+  if (rawConfig === null) {
+    console.error('catalog-config property not specified');
+    return;
   }
-})();
+
+  const config = new CatalogConfig(rawConfig);
+  const app = createApp(SimpleCatalog, { config });
+
+  const i18n = createI18n({
+    locale: config.language,
+    fallbackLocale: AvailableLanguages.itIT,
+    messages,
+  });
+  app.use(i18n);
+
+  app.mount(entry);
+}
+
+const entry = document.querySelector('#vue-catalog');
+if (entry) {
+  initCatalog(entry);
+}
+
+for (const entry of document.querySelectorAll('.vue-simple-catalog')) {
+  initSimpleCatalog(entry);
+}
