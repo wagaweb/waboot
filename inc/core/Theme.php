fe61aab9030575ade877d837cce36bb8831c8e63
LostCore <lostcore@gmail.com>
2023-02-02 11:07:36 +0100

    Theme core updates (logger and registerCommand)

--- inc/core/Theme.php
+++ inc/core/Theme.php
@@ -2,37 +2,52 @@
 
 namespace Waboot\inc\core;
 
-use Symfony\Component\Yaml\Yaml;
 use Waboot\inc\core\mvc\HTMLView;
-use Waboot\inc\core\utils\Utilities;
+use function Waboot\inc\getDefaultTimeZoneName;
 
 class Theme{
-    /**
-     * @var Layout
-     */
-    private $layoutHandler;
-    /**
-     * @var AssetsManager
-     */
-    private $assetsManager;
+	public const LOG_LEVEL_DEBUG = 0;
+	public const LOG_LEVEL_INFO = 1;
+	public const LOG_LEVEL_NOTICE = 2;
+	public const LOG_LEVEL_WARNING = 3;
+	public const LOG_LEVEL_ERROR = 4;
+	public const LOG_LEVEL_CRITICAL = 5;
+	public const LOG_LEVEL_ALERT = 6;
+	public const LOG_LEVEL_EMERGENCY = 7;
+    private ?Layout $layoutHandler = null;
+    private ?AssetsManager $assetsManager = null;
+	private array $registeredFileLoggers;
 
-    public function __construct(AssetsManager $assetsManager, Layout $layout)
+	/**
+	 * @param AssetsManager $assetsManager
+	 * @param Layout $layout
+	 */
+	public function __construct(AssetsManager $assetsManager, Layout $layout)
     {
         $this->assetsManager = $assetsManager;
         $this->layoutHandler = $layout;
     }
 
-    public function getAssetsManager(): AssetsManager
+	/**
+	 * @return AssetsManager
+	 */
+	public function getAssetsManager(): AssetsManager
     {
         return $this->assetsManager;
     }
 
-    public function getLayoutHandler(): Layout
+	/**
+	 * @return Layout
+	 */
+	public function getLayoutHandler(): Layout
     {
         return $this->layoutHandler;
     }
 
-    public function loadDependecies()
+	/**
+	 * @return void
+	 */
+	public function loadDependecies(): void
     {
         $deps = [
             'inc/core/hooks.php',
@@ -67,4 +82,85 @@ class Theme{
             echo $e->getMessage();
         }
     }
+
+	/**
+	 * @param string $command
+	 * @param string|callable $callable
+	 * @param string $prefix
+	 * @param array $description
+	 * @return void
+	 * @throws ThemeException
+	 */
+	public function registerCommand(string $command, $callable, string $prefix = '', array $description = []): void
+	{
+		if(!class_exists('\WP_CLI')){
+			return;
+		}
+		try{
+			if($prefix !== ''){
+				$command = $prefix.':'.$command;
+			}
+			if(empty($description)){
+				if(class_exists($callable) && method_exists($callable,'getCommandDescription')){
+					$description = $callable::getCommandDescription();
+				}
+			}
+			if(\is_array($description) && !empty($description)){
+				\WP_CLI::add_command($command,$callable,$description);
+			}else{
+				\WP_CLI::add_command($command,$callable);
+			}
+		}catch (\Exception | \Throwable $e){
+			throw new ThemeException($e->getMessage());
+		}
+	}
+
+	/**
+	 * @param string $loggerIdentifier
+	 * @param string $logMessage
+	 * @param int $logLevel
+	 * @param array $context
+	 * @param \DateTimeZone|null $dz
+	 * @return void
+	 */
+	public function logToFile(string $loggerIdentifier, string $logMessage, int $logLevel = self::LOG_LEVEL_INFO, array $context = [], \DateTimeZone $dz = null)
+	{
+		try{
+			if($dz === null){
+				$dz = new \DateTimeZone(getDefaultTimeZoneName());
+			}
+			$logger = $this->registeredFileLoggers[$loggerIdentifier] ?? null;
+			if($logger === null){
+				$logFile = WP_CONTENT_DIR.'/logs/'.$loggerIdentifier.'-'.(new \DateTime('now', $dz))->format('Y-m-d').'.log';
+				$logger = LoggerFactory::create('user-update', $logFile);
+				$this->registeredFileLoggers[$loggerIdentifier] = $logger;
+			}
+			switch($logLevel){
+				case self::LOG_LEVEL_DEBUG:
+					$logger->debug($logMessage,$context);
+					break;
+				case self::LOG_LEVEL_INFO:
+					$logger->info($logMessage,$context);
+					break;
+				case self::LOG_LEVEL_NOTICE:
+					$logger->notice($logMessage,$context);
+					break;
+				case self::LOG_LEVEL_WARNING:
+					$logger->warning($logMessage,$context);
+					break;
+				case self::LOG_LEVEL_ERROR:
+					$logger->error($logMessage,$context);
+					break;
+				case self::LOG_LEVEL_CRITICAL:
+					$logger->critical($logMessage,$context);
+					break;
+				case self::LOG_LEVEL_ALERT:
+					$logger->alert($logMessage,$context);
+					break;
+				case self::LOG_LEVEL_EMERGENCY:
+					$logger->emergency($logMessage,$context);
+					break;
+			}
+		}catch (\Exception | \Throwable $e){}
+	}
 }
---
+++ inc/core/ThemeException.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace Waboot\inc\core;
+
+class ThemeException extends \Exception
+{}
