ce86e261ef86edf31760848dea1dc593861cee1c
LostCore <lostcore@gmail.com>
2023-02-16 15:58:02 +0100

    createFromFormat and fix

--- inc/core/utils/Dates.php
+++ inc/core/utils/Dates.php
@@ -5,6 +5,24 @@ namespace Waboot\inc\core\utils;
 class Dates
 {
     /**
+     * @param string $format
+     * @param string $string
+     * @param \DateTimeZone|null $tz
+     * @return \DateTime|null
+     */
+    public static function createFromFormat(string $format, string $string, \DateTimeZone $tz = null): ?\DateTime
+    {
+        if(!$tz){
+            $tz = self::getDefaultDateTimeZone();
+        }
+        $dt = date_create_from_format($format,$string,$tz);
+        if(!$dt instanceof \DateTime){
+            return null;
+        }
+        return $dt;
+    }
+
+    /**
      * @param \DateTimeZone|null $tz
      * @return \DateTime
      * @throws \Exception
@@ -43,6 +61,11 @@ class Dates
      */
     public static function getDefaultDateTimeZone(): \DateTimeZone
     {
-        return new \DateTimeZone(date_default_timezone_get() ?: 'UTC');
+        if(function_exists('wc_timezone_string')){
+            $tz = wc_timezone_string();
+        }else{
+            $tz = date_default_timezone_get() ?? 'UTC';
+        }
+        return new \DateTimeZone($tz);
     }
 }
