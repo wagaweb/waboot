8ccb6381239cc69972d336866c8a7ee49a4a7862
Damiano Todeschini <damiano@waga.it>
2024-03-19 17:28:25 +0100

    footers styles fixes + render block improvement

--- addons/packages/footer/templates/footerTemplate.php
+++ addons/packages/footer/templates/footerTemplate.php
@@ -1,10 +1,8 @@
 <div class="footer__inner">
 	<?php
 		if (!empty($footerBlock)) {
-			$blockContent = parse_blocks(get_post_field('post_content', $footerBlock));
-			if (!empty($blockContent)) {
-				echo render_block($blockContent[0]);
-			}
+			echo apply_filters('the_content', get_post_field('post_content', $footerBlock));
 		}
+		
 	?>
 </div>
