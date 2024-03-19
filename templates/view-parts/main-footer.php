a586a0b1966665a2e70dc70d1243ee55338366c9
Damiano Todeschini <damiano@waga.it>
2024-03-19 17:00:32 +0100

    footers styles fixes

--- templates/view-parts/main-footer.php
+++ templates/view-parts/main-footer.php
@@ -1,27 +1,7 @@
 <div class="footer__closure">
-    <div class="footer__left">
-        <div class="footer__copy">© <?php echo date( 'Y' ); ?> <?php echo get_bloginfo(); ?> | All Rights Reserved.</div>
-    </div>
+    <div class="footer__copy">© <?php echo date( 'Y' ); ?> <?php echo get_bloginfo(); ?> | All Rights Reserved.</div>
 
-    <div class="footer__right">
-        <?php /*wp_nav_menu( [
-            'theme_location' => 'social',
-            'depth' => 0,
-            'fallback_cb' => '__return_false',
-            'container' => false,
-            'menu_class' => apply_filters('waboot/navigation/main/class', 'footer__menu footer__menu--social')
-        ] );
-        */?>
+    <div class="footer__payments">
         <img src="<?php echo get_theme_file_uri('/assets/images/payment-methods.svg') ?>" alt="Payment Methods">
     </div>
 </div>
-
-
-<?php
-/*wp_nav_menu( [
-    'theme_location' => 'bottom',
-    'depth' => 0,
-    'fallback_cb' => '__return_false',
-    'container' => false,
-    'menu_class' => apply_filters('waboot/navigation/main/class', 'footer__menu footer__menu--bottom')
-] ); */?>
