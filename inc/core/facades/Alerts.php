2793a0d5b929c07dce3e2ad3c7f3c3d631f75a03
LostCore <lostcore@gmail.com>
2024-07-08 17:13:37 +0200

    Alerts facades

---
+++ inc/core/facades/Alerts.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Waboot\inc\core\facades;
+
+use Waboot\inc\core\alert\AlertDispatcher;
+use Waboot\inc\core\alert\Alert;
+use Waboot\inc\core\alert\AlertDispatcherException;
+use Waboot\inc\core\alert\AlertException;
+
+class Alerts
+{
+    static function dispatchEmailAlert(string $title, string $message, string $recipient, \DateTimeZone $tz = null){
+        try {
+            $ad = new AlertDispatcher('export-rma-ad',AlertDispatcher::DISPATCH_METHOD_EMAIL,$recipient);
+            $id = base64_encode($title.$message.$recipient);
+            $ad->addAlert(new Alert($id,$title,$message,$tz));
+            $ad->dispatch();
+        } catch (AlertException|AlertDispatcherException $e) {
+            error_log('Alerts::dispatchEmailAlert ERROR: '.$e->getMessage());
+        }
+    }
+}
