4c62ce3e1421f86b72f664df13423f92bbc1c6f5
lostcore <lostcore@gmail.com>
2024-09-19 11:02:09 +0200

    ref genfeeds.sh

--- bin/genfeeds.sh
+++ bin/genfeeds.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+BASEDIR=/srv/www/waboot
+
+if [ "$BASEDIR" = "/srv/www/waboot" ]; then
+    echo "Base dir is still /srv/www/waboot!"
+    exit 1
+fi
+
 echo "GShopping (it)..."
 
-(cd /srv/www/waboot && /usr/local/bin/wp wawoo:feeds:generate-gshopping --lang=it --default-google-product-category="Apparel & Accessories" )
+(cd $BASEDIR && /usr/local/bin/wp wawoo:feeds:generate-gshopping --lang=it --default-google-product-category="Apparel & Accessories" )
 
 echo "Facebook (it)..."
 
-( rm /srv/www/waboot/wp-content/waboot-feeds/facebook-products-feed-it.xml )
+( rm $BASEDIR/wp-content/wb-feeds/facebook-products-feed-it.xml )
 
-( cp /srv/www/waboot/wp-content/waboot-feeds/google-products-feed-it.xml /srv/www/waboot/wp-content/waboot-feeds/facebook-products-feed-it.xml )
+( cp $BASEDIR/wp-content/wb-feeds/google-products-feed-it.xml $BASEDIR/wp-content/wb-feeds/facebook-products-feed-it.xml )
 
 echo "Done!"
