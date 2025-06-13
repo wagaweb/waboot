87e2f768d197c6c19bcac058d1eefdaed60e4727
lostcore <lostcore@gmail.com>
2025-06-13 13:14:58 +0200

    Refactor checkout

--- addons/packages/checkout/step_checkout_advanced/assets/src/components/Breadcrumb.vue
+++ addons/packages/checkout/step_checkout_advanced/assets/src/components/Breadcrumb.vue
@@ -1,24 +1,38 @@
 <script setup lang="ts">
 import {useBreadCrumbStore} from "@/stores/breadcrumb";
+import {computed} from "vue";
 
 const breadCrumbStore = useBreadCrumbStore();
+
+const currentActiveStep = computed(() => {
+    switch(breadCrumbStore.currentStep){
+        case 1:
+        case 2:
+            return 'login';
+        case 3:
+            return 'shipping'
+        case 4:
+            return 'payment'
+    }
+});
+
 </script>
 <template>
   <ul class="woocommerce-checkout-steps__list">
     <li class="woocommerce-checkout-steps__item"
-        :class="{'woocommerce-checkout-steps__item--current': breadCrumbStore.currentStep === 1, 'woocommerce-checkout-steps__item--done': breadCrumbStore.currentStep <= 3}">
+        :class="{'woocommerce-checkout-steps__item--current': currentActiveStep === 'login', 'woocommerce-checkout-steps__item--done': currentActiveStep !== 'login' }">
       <div class="woocommerce-checkout-steps__link">
         <span class="woocommerce-checkout-steps__label">{{ $t('Login') }}</span>
       </div>
     </li>
     <li class="woocommerce-checkout-steps__item"
-        :class="{'woocommerce-checkout-steps__item--current': breadCrumbStore.currentStep === 2, 'woocommerce-checkout-steps__item--done': breadCrumbStore.currentStep > 1}">
+        :class="{'woocommerce-checkout-steps__item--current': currentActiveStep === 'shipping', 'woocommerce-checkout-steps__item--done': currentActiveStep !== 'shipping'}">
       <div class="woocommerce-checkout-steps__link">
         <span class="woocommerce-checkout-steps__label">{{ $t('Shipping') }}</span>
       </div>
     </li>
     <li class="woocommerce-checkout-steps__item"
-        :class="{'woocommerce-checkout-steps__item--current': breadCrumbStore.currentStep === 3, 'woocommerce-checkout-steps__item--done': breadCrumbStore.currentStep === 3}">
+        :class="{'woocommerce-checkout-steps__item--current': currentActiveStep === 'payment', 'woocommerce-checkout-steps__item--done': currentActiveStep !== 'payment'}">
       <div class="woocommerce-checkout-steps__link">
         <span class="woocommerce-checkout-steps__label">{{ $t('Payment') }}</span>
       </div>
