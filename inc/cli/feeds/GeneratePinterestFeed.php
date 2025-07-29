86c78ca19790800b52a92e2924c276cf7cc432cb
lostcore <lostcore@gmail.com>
2025-07-29 15:20:17 +0200

    Ref gen feeds

--- inc/cli/feeds/GeneratePinterestFeed.php
+++ inc/cli/feeds/GeneratePinterestFeed.php
@@ -2,6 +2,10 @@
 
 namespace Waboot\inc\cli\feeds;
 
+// https://business.pinterest.com/it/blog/how-to-optimize-product-feed/
+// https://help.pinterest.com/en/business/article/before-you-get-started-with-catalogs#section-9441
+// https://help.pinterest.com/sub/helpcenter/assets/pinterest_product_sample_xml_feed.xml.zip
+
 class GeneratePinterestFeed extends GenerateGShoppingFeed
 {
     protected string $customOutputFilename = 'pinterest-products-feed';
