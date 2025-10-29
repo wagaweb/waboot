7628f02a201bf04ab8485c1639f0f929966eadbe
lostcore <lostcore@gmail.com>
2025-10-29 14:53:41 +0100

    Introduced CustomerRepository.php

--- inc/core/woocommerce/repositories/CustomerRepository.php
+++ inc/core/woocommerce/repositories/CustomerRepository.php
@@ -5,7 +5,9 @@ namespace Waboot\inc\core\woocommerce\repositories;
 use Illuminate\Database\Capsule\Manager;
 use Waboot\inc\core\DBException;
 use waboot\inc\core\woocommerce\Customer;
+use Waboot\inc\core\woocommerce\CustomerException;
 use Waboot\inc\core\woocommerce\CustomerFactory;
+use Waboot\inc\core\woocommerce\CustomerNotFoundException;
 use function Waboot\inc\core\helpers\Waboot;
 
 class CustomerRepository
@@ -22,6 +24,12 @@ class CustomerRepository
         }
     }
 
+    /**
+     * @param int $id
+     * @return Customer|null
+     * @throws CustomerException
+     * @throws CustomerNotFoundException
+     */
     public function find(int $id): ?Customer
     {
         $user = get_user_by('id', $id);
@@ -34,6 +42,8 @@ class CustomerRepository
     /**
      * @param string $email
      * @return Customer|null
+     * @throws CustomerException
+     * @throws CustomerNotFoundException
      */
     public function findByEmail(string $email): ?Customer
     {
