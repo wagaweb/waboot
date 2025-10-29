42084592b3b835f48a6d0bdd0691a5a3bcb7b95f
lostcore <lostcore@gmail.com>
2025-10-29 15:00:16 +0100

    Introduced AbstractRepository.php

--- inc/core/woocommerce/repositories/CustomerRepository.php
+++ inc/core/woocommerce/repositories/CustomerRepository.php
@@ -2,36 +2,26 @@
 
 namespace Waboot\inc\core\woocommerce\repositories;
 
-use Illuminate\Database\Capsule\Manager;
-use Waboot\inc\core\DBException;
+use Waboot\inc\core\repositories\AbstractRepository;
 use waboot\inc\core\woocommerce\Customer;
 use Waboot\inc\core\woocommerce\CustomerException;
 use Waboot\inc\core\woocommerce\CustomerFactory;
 use Waboot\inc\core\woocommerce\CustomerNotFoundException;
-use function Waboot\inc\core\helpers\Waboot;
 
-class CustomerRepository
+class CustomerRepository extends AbstractRepository
 {
-    private Manager $db;
-
     /**
-     * @throws DBException
-     */
-    public function __construct(Manager $queryBuilder = null)
-    {
-        if(!$queryBuilder){
-            $this->db = Waboot()->DB()->getQueryBuilder();
-        }
-    }
-
-    /**
-     * @param int $id
+     * @param int|string $id
      * @return Customer|null
      * @throws CustomerException
      * @throws CustomerNotFoundException
      */
-    public function find(int $id): ?Customer
+    public function find(int|string $id): ?Customer
     {
+        if(!is_numeric($id)) {
+            return null;
+        }
+        $id = (int) $id;
         $user = get_user_by('id', $id);
         if(!$user instanceof \WP_User) {
             return null;
