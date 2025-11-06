d4db05a108d28c876ea3a266c52637ce8d5d0834
lostcore <lostcore@gmail.com>
2025-11-06 11:04:17 +0100

    Sync with ecommerce

--- inc/core/facades/repositories.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Waboot\inc\core\facades;
-
-use Waboot\inc\core\woocommerce\repositories\CustomerRepository;
-
-function customer(): CustomerRepository {
-    return new CustomerRepository();
-}
