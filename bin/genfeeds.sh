d61d1ed57480cb1470a931598ec45edaab80c4ce
lostcore <lostcore@gmail.com>
2026-02-12 12:13:32 +0100

    nota per cron

--- bin/genfeeds.sh
+++ bin/genfeeds.sh
@@ -7,6 +7,9 @@ if [ "$BASEDIR" = "/srv/www/waboot" ]; then
     exit 1
 fi
 
+# Ansible: Generate feeds
+# 0 5 * * * /home/waboot/bin/genfeeds >> /home/waboot/logs/cli_genfeeds-$(date +\%Y-\%m-\%d).log 2>&1
+
 echo "GShopping (it)..."
 
 (cd $BASEDIR && /usr/local/bin/wp wawoo:feeds:generate-gshopping --lang=it --default-google-product-category="Apparel & Accessories" )
