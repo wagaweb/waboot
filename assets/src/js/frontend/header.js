e33cd6adc9fe93ea3ddad547c1913e166d00c7f6
Gennaro Di Fiandra <gennarodifiandra@gmail.com>
2026-05-12 12:08:09 +0200

    Add role and aria-expanded to menu item with submenu.

--- assets/src/js/frontend/header.js
+++ assets/src/js/frontend/header.js
@@ -6,7 +6,7 @@ export default class {
         this.headerFixed();
         this.backOnSubmenu();
         this.mobileDropdown(el);
-        this.escapeSubmenu();
+        this.accessibleSubmenu();
 
         $(window).on("scroll", ()=> {
             this.headerFixed();
@@ -52,39 +52,54 @@ export default class {
 
     mobileDropdown(el) {
         if($(el).length > 0) {
-            $(el + ' > .sublevel__icon').on('click', function (e) {
+            $(el + ' > .sublevel__icon').on('click', function(e) {
                 e.preventDefault();
                 e.stopPropagation();
                 let $target = $(e.currentTarget),
                     $submenu = $target.prev('.sub-menu');
-                $submenu.css('left',0);
+                $submenu.css('left', 0);
+                $target.siblings('a').attr('aria-expanded', 'true');
             });
-            $(el + ' > ul .backlevel__icon').on('click', function (e) {
+
+            $(el + ' > ul .backlevel__icon').on('click', function(e) {
                 e.preventDefault();
                 e.stopPropagation();
                 let $target = $(e.currentTarget).parent('ul');
-                $target.css('left','100%');
+                $target.css('left', '100%');
+                $target.closest('.menu-item-has-children').children('a').attr('aria-expanded', 'false');
             });
-            $('[data-slidein-close]').on('click', function(){
-                $('.navigation-mobile .sub-menu').css('left','100%');
+
+            $('[data-slidein-close]').on('click', function() {
+                $('.navigation-mobile .sub-menu').css('left', '100%');
+                $('.navigation-mobile .menu-item-has-children > a').attr('aria-expanded', 'false');
             });
         }
-    };
+    }
 
-    escapeSubmenu() {
+    accessibleSubmenu() {
         const $items = $('.navigation.navbar-nav .menu-item-has-children');
+
+        $items.each(function() {
+            $(this).children('a').attr({
+                'role': 'button',
+                'aria-expanded': 'false'
+            });
+        });
 
         $items.on('mouseenter', function() {
             $(this).addClass('is-hovered');
+            $(this).children('a').attr('aria-expanded', 'true');
         });
 
         $items.on('mouseleave', function() {
             $(this).removeClass('is-hovered');
+            $(this).children('a').attr('aria-expanded', 'false');
         });
 
         $(document).on('keydown', function(e) {
             if (e.key === 'Escape') {
                 $items.removeClass('is-hovered');
+                $items.children('a').attr('aria-expanded', 'false');
             }
         });
     }
