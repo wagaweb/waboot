c34508c036b210fa91c318f3119f4e2c9d0c8c11
Gennaro Di Fiandra <gennarodifiandra@gmail.com>
2026-05-13 11:16:59 +0200

    Add skip to content.

--- header.php
+++ header.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
     <?php \Waboot\inc\site_head(); ?>
 </head>
-<body <?php body_class(); ?> >
 
-<?php do_action('waboot/layout/page-before'); ?>
+<body <?php body_class(); ?>>
 
-<header class="header">
+    <?php do_action('waboot/layout/page-before'); ?>
 
-    <?php do_action('waboot/layout/header'); ?>
+    <header class="header">
 
-</header>
+        <a class="skip-link sr-only sr-only-focusable" href="#main">
+            <?php _e('Skip to content', LANG_TEXTDOMAIN); ?>
+        </a>
 
+        <?php do_action('waboot/layout/header'); ?>
 
+    </header>
