cd0664a4f045f6795e6a6d0275881df2dca195f0
Gennaro Di Fiandra <gennarodifiandra@gmail.com>
2026-05-13 11:26:11 +0200

    Move navigation mobile into header.

--- inc/hooks/layout.php
+++ inc/hooks/layout.php
@@ -4,23 +4,24 @@ namespace Waboot\inc\hooks;
 
 use function Waboot\inc\core\Waboot;
 
-add_action('waboot/layout/header', function(){
+add_action('waboot/layout/header', function () {
     Waboot()->renderView('templates/view-parts/main-header.php');
-});
-
-add_action('waboot/layout/footer', function(){
-    Waboot()->renderView('templates/view-parts/main-footer.php');
-});
-
-add_action('waboot/layout/page-after', function(){
     Waboot()->renderView('templates/view-parts/navigation-mobile.php');
 });
 
-add_action("waboot/widget_area/footer/before", function(){
+add_action('waboot/layout/footer', function () {
+    Waboot()->renderView('templates/view-parts/main-footer.php');
+});
+
+add_action('waboot/layout/page-after', function () {
+    // Waboot()->renderView('templates/view-parts/navigation-mobile.php');
+});
+
+add_action("waboot/widget_area/footer/before", function () {
     echo '<div class="widgetarea__footer-inner">';
 });
 
-add_action("waboot/widget_area/footer/after", function(){
+add_action("waboot/widget_area/footer/after", function () {
     echo '</div>';
 });
 
